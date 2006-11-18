1d824d0353b77b3b0fef9b2fa5b8eeadea49a012
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2006-11-18 01:37:15 +0000

    add sepgsql.h

---
+++ trunk/src/include/sepgsql.h
@@ -0,0 +1,15 @@
+/*
+ * src/include/sepgsql.h
+ *    The header file of Security Enhanced PostgreSQL
+ *
+ * Copyright (c) 2006 - 2007, KaiGai Kohei <kaigai@kaigai.gr.jp>
+ */
+#ifndef SEPGSQL_H
+#define SEPGSQL_H
+
+#ifdef HAVE_SELINUX
+
+#else
+
+#endif /* HAVE_SELINUX */
+#endif /* SEPGSQL_H */
