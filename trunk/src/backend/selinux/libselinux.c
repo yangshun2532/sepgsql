a934f027e09136f5e8d87f57f33c2fb999b7e261
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2006-11-18 01:54:17 +0000

    add a header file into libselinux.c to avoid compiler warnning.

--- trunk/src/backend/selinux/libselinux.c
+++ trunk/src/backend/selinux/libselinux.c
@@ -9,6 +9,7 @@
 #include "access/heapam.h"
 #include "access/genam.h"
 #include "access/tupdesc.h"
+#include "access/xact.h"
 #include "catalog/indexing.h"
 #include "catalog/pg_selinux.h"
 #include "miscadmin.h"
