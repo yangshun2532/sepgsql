ecd13a091c6cb3bae9add81269701d2c1eb69564
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2006-11-26 10:26:09 +0000

    add src/backend/selinux/procedure.c  (It should be merged at previous version :-(...

---
+++ trunk/src/backend/selinux/procedure.c
@@ -0,0 +1,52 @@
+/*
+ * src/backend/selinux/procedure.c
+ *    The procedure supportings for SE-PostgreSQL
+ * Copyright (c) 2006 - 2007 KaiGai Kohei <kaigai@kaigai.gr.jp>
+ */
+#include "postgres.h"
+
+#include "catalog/pg_database.h"
+#include "sepgsql.h"
+
+#include <catalog/pg_proc.h>
+#include <fmgr.h>
+#include <miscadmin.h>
+#include <selinux/flask.h>
+#include <selinux/av_permissions.h>
+#include <utils/syscache.h>
+
+Query *selinuxProxyCreateProcedure(Query *query)
+{
+	psid ppsid;
+	int rc;
+	char *audit;
+
+	/* compute the context of newly created procedure */
+	ppsid = libselinux_avc_createcon(selinuxGetClientPsid(),
+									 selinuxGetDatabasePsid(),
+									 SECCLASS_PROCEDURE);
+
+	/* 1. check database:create_obj permission */
+	rc = libselinux_avc_permission(selinuxGetClientPsid(),
+								   selinuxGetDatabasePsid(),
+								   SECCLASS_DATABASE,
+								   DATABASE__CREATE_OBJ, &audit);
+	selinux_audit(rc, audit, NULL);
+
+	/* 2. check procedure:create permission */
+	rc = libselinux_avc_permission(selinuxGetClientPsid(),
+								   ppsid, SECCLASS_PROCEDURE,
+								   PROCEDURE__CREATE, &audit);
+	selinux_audit(rc, audit, NULL);
+
+	return query;
+}
+
+void selinuxHookCreateProcedure(Datum *values, char *nulls)
+{
+	psid ppsid = libselinux_avc_createcon(selinuxGetClientPsid(),
+										  selinuxGetDatabasePsid(),
+										  SECCLASS_PROCEDURE);
+	values[Anum_pg_proc_proselcon - 1] = ObjectIdGetDatum(ppsid);
+	nulls[Anum_pg_proc_proselcon - 1] = ' ';
+}
