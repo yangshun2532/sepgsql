bc1a5f987a1ec8d9d7e276aeb0980b72b17d6203
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2007-06-17 11:39:49 +0000

    remove obsolete branch

--- branches/check-one-phase/contrib/pg_freespacemap/uninstall_pg_freespacemap.sql
+++
@@ -1,7 +0,0 @@
-SET search_path = public;
-
-DROP VIEW pg_freespacemap_pages;
-DROP VIEW pg_freespacemap_relations;
-
-DROP FUNCTION pg_freespacemap_pages();
-DROP FUNCTION pg_freespacemap_relations();
--- branches/check-one-phase/src/include/port/linux.h
+++
@@ -1,1 +0,0 @@
-/* $PostgreSQL: pgsql/src/include/port/linux.h,v 1.42 2006/10/04 00:30:09 momjian Exp $ */
