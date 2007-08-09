04e1474b985f9d483a815ead08403a220de01933
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2007-08-09 06:50:22 +0000

    - Remove Fedora 6 support - add "db_" prefix onto object classes

--- scripts/build-policy.sh
+++ scripts/build-policy.sh
@@ -15,11 +15,7 @@ else
 fi
 
 # add patches
-if rpm -qp --qf "%{release}" "${SRPMFILE}" | egrep -q '\.fc6$'; then
-    # Fedore core 6
-    POLICY_98="refpolicy-add-sepgsql-definitions.fc6.patch"
-    POLICY_99="refpolicy-add-userdomain-pgsql-connect.fc6.patch"
-elif rpm -qp --qf "%{release}" "${SRPMFILE}" | egrep -q '\.fc7$'; then
+if rpm -qp --qf "%{release}" "${SRPMFILE}" | egrep -q '\.fc7$'; then
     # Fedora 7
     POLICY_98="refpolicy-add-sepgsql-definitions.fedora7.patch"
     POLICY_99="refpolicy-add-userdomain-pgsql-connect.fedora7.patch"
