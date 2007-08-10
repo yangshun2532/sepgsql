bf8d6cdf36cb0eff6f9ac5122843b9eff3d8b756
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2007-08-10 04:34:21 +0000

    update build-policy.sh  - update Makefile.devel in rawhide

--- scripts/build-policy.sh
+++ scripts/build-policy.sh
@@ -14,64 +14,68 @@ else
     SRPMFILE=$1
 fi
 
-# add patches
+# unpack source rpm
+SOURCE_DIR=`rpm -E '%{_sourcedir}'`
+if [ ! -d ${SOURCE_DIR} ]; then
+    echo "${SOURCE_DIR} is not a directory"
+    exit 1
+fi
+
+cd ${SOURCE_DIR}
+rm -f ${SOURCE_DIR}/*
+rpm2cpio ${SRPMFILE} | cpio -id || exit 1
+if [ ! -f selinux-policy.spec ]; then
+    echo "selinux-policy.spec not found";
+    exit 1
+fi
+
+# export repository
+POLICY_REPO="`mktemp -d`/policy"
+svn export "${SEPGSQL_REPOSITORY}/policy" ${POLICY_REPO}
+
+# modify specfile
 if rpm -qp --qf "%{release}" "${SRPMFILE}" | egrep -q '\.fc7$'; then
     # Fedora 7
-    POLICY_98="refpolicy-add-sepgsql-definitions.fedora7.patch"
-    POLICY_99="refpolicy-add-userdomain-pgsql-connect.fedora7.patch"
+    TEMPFILE=`mktemp`
+    cp ${POLICY_REPO}/refpolicy-add-sepgsql-definitions.fedora7.patch ${SOURCE_DIR}
+    cp ${POLICY_REPO}/refpolicy-add-userdomain-pgsql-connect.fedora7.patch ${SOURCE_DIR}
+    cat selinux-policy.spec                                                                     \
+        | sed 's/%{?dist}/.sepgsql%{?dist}/g'                                                   \
+        | awk "BEGIN { a = b = 0; }                                                             \
+               /^patch[0-9]*:/ { a++; print; next; }                                            \
+               a > 0 { a = 0;                                                                   \
+                       print \"patch98: refpolicy-add-sepgsql-definitions.fedora7.patch\";      \
+                       print \"patch99: refpolicy-add-userdomain-pgsql-connect.fedora7.patch\"; \
+                       print; next; }                                                           \
+               /^%patch[0-9]*/ { b++; print; next; }                                            \
+               b > 0 { b = 0;                                                                   \
+                       print \"%patch98 -p1\"                                                   \
+                       print \"%patch99 -p1\"                                                   \
+                       print; next; }                                                           \
+               { print; }" > $TEMPFILE
+    diff -NU3 selinux-policy.spec $TEMPFILE | less
 elif rpm -qp --qf "%{release}" "${SRPMFILE}" | egrep -q '\.fc8$'; then
     # Fedora 8 (rawhide)
-    POLICY_98="refpolicy-add-sepgsql-definitions.fedora8.patch"
+    TEMPFILE=`mktemp`
+    cp ${POLICY_REPO}/refpolicy-add-sepgsql-definitions.fedora8.patch ${SOURCE_DIR}
+    cp ${POLICY_REPO}/refpolicy-fixed-Makefile.devel-fedora8 ${SOURCE_DIR}/Makefile.devel
+    cat selinux-policy.spec                                                                     \
+        | sed 's/%{?dist}/.sepgsql%{?dist}/g'                                                   \
+        | awk "BEGIN { a = b = 0; }                                                             \
+               /^patch[0-9]*:/ { a++; print; next; }                                            \
+               a > 0 { a = 0;                                                                   \
+                       print \"patch99: refpolicy-add-sepgsql-definitions.fedora8.patch\";      \
+                       print; next; }                                                           \
+               /^%patch[0-9]*/ { b++; print; next; }                                            \
+               b > 0 { b = 0;                                                                   \
+                       print \"%patch99 -p1\";                                                  \
+                       print; next; }                                                           \
+               { print; }" > $TEMPFILE
+    diff -NU3 selinux-policy.spec $TEMPFILE | less
 else
     echo "unknown distribution: ${SRPMFILE}"
     exit 1
 fi
-
-# unpack source rpm
-SOURCE_DIR=`rpm -E '%{_sourcedir}'`
-test -d ${SOURCE_DIR} || { echo "${SOURCE_DIR} not a directory"; exit 1; }
-cd ${SOURCE_DIR}
-rm ${SOURCE_DIR}/*
-rpm2cpio ${SRPMFILE} | cpio -id || exit 1
-test -f ${SOURCE_DIR}/selinux-policy.spec || \
-    { echo "selinux-policy.spec not found"; exit 1; }
-
-# fetch patches from repository
-TEMPDIR=`mktemp -d`
-svn export "${SEPGSQL_REPOSITORY}/policy" ${TEMPDIR}/policy
-set | grep ^POLICY_ | 
-
-
-PATCH_DEF=""
-PATCH_APPLY=""
-LIST=`set | grep ^POLICY_ | sed 's/POLICY_//g'`
-for x in $LIST
-do
-    PATCH_NO=`echo $x | sed 's/=/ /g' | awk '{ print $1 }'`
-    PATCH_FILE=`echo $x | sed 's/=/ /g' | awk '{ print $2 }'`
-    echo $PATCH_NO | egrep -q '^[0-9]+$' || \
-        { echo "${PATCH_NO} not a numerical value"; exit 1; }
-    test -f ${TEMPDIR}/policy/${PATCH_FILE} || \
-        { echo "${PATCH_FILE} not found"; exit 1; }
-
-    cp ${TEMPDIR}/policy/${PATCH_FILE} ${SOURCE_DIR}
-    PATCH_DEF="${PATCH_DEF} print(\"patch${PATCH_NO}: ${PATCH_FILE}\");"
-    PATCH_APPLY="${PATCH_APPLY} print(\"%patch${PATCH_NO} -p1\");"
-done
-rm -rf ${TEMPDIR}
-
-# modify spec file
-_SPEC=`mktemp`
-cat ${SOURCE_DIR}/selinux-policy.spec                   \
-    | sed 's/%{?dist}/.sepgsql%{?dist}/g'               \
-    | awk "BEGIN { a = b = 0; }                         \
-           /^patch[0-9]*:/ { a++; print; next; }        \
-           a > 0 { a=0; ${PATCH_DEF}; print; next; }    \
-           /^%patch[0-9]*/ { b++; print; next; }        \
-           b > 0 { b=0; ${PATCH_APPLY}; print; next; }  \
-           { print }" > ${_SPEC}
-cp ${_SPEC} ${SOURCE_DIR}/selinux-policy.spec
-rm -f ${_SPEC}
 
 # rpmbuild 
 rpmbuild -ba ${SOURCE_DIR}/selinux-policy.spec
