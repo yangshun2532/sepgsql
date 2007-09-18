38da59612f831ccf8196822434b5356a1d4e37e2
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2007-09-18 01:46:45 +0000

    add pgsql-pull-cvs.sh to sync with PostgreSQL CVS tree.

---
+++ scripts/pgsql-pull-cvs.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# This script pulls the PostgreSQL tree from CVS.
+# ---- parametors ----
+CVSTAG="HEAD"
+SVNBRANCH="/trunk"
+
+if [ -d "${SEPGSQL_REPOSITORY}" ]; then
+    svn info "${SEPGSQL_REPOSITORY}" || exit 1
+else
+    echo "could not find SVN WC"
+    exit 1
+fi
+
+# ---- make a working directory
+WORKDIR=`mktemp -d`
+cd ${WORKDIR} || exit 1
+
+# ---- export CVS ----
+cvs -z3 -d :pserver:anoncvs@anoncvs.postgresql.org:/projects/cvsroot \
+    export -r ${CVSTAG} -d pgsql.cvs pgsql
+test $? -eq 0 || exit 1
+
+rm -f pgsql.cvs/configure
+
+# ---- export SVN ----
+svn update ${SEPGSQL_REPOSITORY}
+svn export ${SEPGSQL_REPOSITORY}${SVNBRANCH}/base pgsql.svn
+test $? -eq 0 || exit 1
+
+SVNREV=`env LANG=C svn info ${SEPGSQL_REPOSITORY} \
+    | grep "^Revision:" \
+    | awk '{ print $2; }'`
+
+# ---- make a diff ----
+diff -prNU3 pgsql.svn pgsql.cvs > pgsql-updates.diff
+(cd pgsql.cvs; find . ) > ./filelist.cvs
+(cd pgsql.svn; find . ) > ./filelist.svn
+diff -NU3 filelist.svn filelist.cvs > pgsql-filelist.diff
+
+# ---- apply diffs ----
+cd ${SEPGSQL_REPOSITORY}${SVNBRANCH}/base
+cat ${WORKDIR}/pgsql-updates.diff | patch -p1
+
+cat ${WORKDIR}/pgsql-filelist.diff | egrep "^\+\./" | while read ENT
+do
+    ENT=`echo $ENT | sed 's/^\+//g'`
+    svn add ${ENT}
+done
+
+cat ${WORKDIR}/pgsql-filelist.diff | egrep "^\-\./" | while read ENT
+do
+    ENT=`echo $ENT | sed 's/^\-//g'`
+    svn del ${ENT}
+done
+
+# ---- End ----
+echo "$0 done, check diff and following commands"
+echo "--------"
+echo "cd ${SEPGSQL_REPOSITORY}${SVNBRANCH}"
+echo "svn diff ./base"
+echo "svn commit -m 'CVS pull -r ${CVSTAG} at `env LANG=C date`' ./base"
+echo "svn update"
+echo
+echo "svn merge -c `expr ${SVNREV} + 1` ./base ./pgace"
+echo "svn diff ./pgace"
+echo "svn commit -m 'update ${SVNBRANCH}/pgace' came from CVS-pull at `env LANG=C date`" ./pgace
+echo "svn update"
+echo "--------"
