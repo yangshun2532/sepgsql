1e2d85ec606b887edc0d6b4783f66d7ff3c19e58
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2007-11-23 05:55:54 +0000

    SE-PostgreSQL/PGACE interface functions are move to sepgsql/interface.c

---
+++ trunk/sepgsql/src/backend/security/sepgsql/interface.c
@@ -0,0 +1,318 @@
+/*
+ * src/backend/security/sepgsql/interface.c
+ *   SE-PostgreSQL/PGACE Interfaces
+ *
+ * Copyright (c) 2007 KaiGai Kohei <kaigai@kaigai.gr.jp>
+ */
+
+/******************************************************************
+ * Initialize / Finalize related hooks
+ ******************************************************************/
+Size pgaceShmemSize(void)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlShmemSize();
+	return (Size) 0;
+}
+
+void pgaceInitialize(bool is_bootstrap)
+{
+	if (sepgsqlIsEnabled())
+		sepgsqlInitialize();
+}
+
+bool pgaceInitializePostmaster(void)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlInitializePostmaster();
+	return true;
+}
+
+void pgaceFinalizePostmaster(void)
+{
+	if (sepgsqlIsEnabled())
+		sepgsqlFinalizePostmaster()
+}
+
+/******************************************************************
+ * SQL proxy hooks
+ ******************************************************************/
+List *pgaceProxyQuery(List *queryList)
+{
+	List *newList = NIL;
+	ListCell *l;
+
+	if (sepgsqlIsEnabled()) {
+		foreach (l, queryList) {
+			Query *q = (Query *) lfirst(l);
+
+			newList = list_concat(newList, sepgsqlProxyQuery(q));
+		}
+		queryList = newList;
+	}
+	return queryList;
+}
+
+void pgacePortalStart(Portal portal)
+{
+	/* do nothing */
+}
+
+void pgaceExecutorStart(QueryDesc *queryDesc, int eflags)
+{
+	if (sepgsqlIsEnabled() && !(eflags & EXEC_FLAG_EXPLAIN_ONLY)) {
+		Assert(queryDesc->parsetree != NULL);
+		sepgsqlVerifyQuery(queryDesc->parsetree);
+	}
+}
+
+/******************************************************************
+ * HeapTuple modification hooks
+ ******************************************************************/
+bool pgaceHeapTupleInsert(Relation rel, HeapTuple tuple,
+						  bool is_internal, bool with_returning)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlHeapTupleInsert(rel, tuple, is_internal, with_returning);
+	return true;
+}
+
+bool pgaceHeapTupleUpdate(Relation rel, ItemPointer otid, HeapTuple newtup,
+						  bool is_internal, bool with_returning)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlHeapTupleUpdate(rel, otid, newtup, is_internal, with_returning);
+	return true;
+}
+
+bool pgaceHeapTupleDelete(Relation rel, ItemPointer otid,
+						  bool is_internal, bool with_returning)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlHeapTupleDelete(rel, otid, is_internal, with_returning);
+	return true;
+}
+
+/******************************************************************
+ * Extended SQL statement hooks
+ ******************************************************************/
+DefElem *pgaceGramSecurityLabel(char *defname, char *value)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlGramSecurityLabel(defname, value);
+	return NULL;
+}
+
+bool pgaceNodeIsSecurityLabel(DefElem *defel)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlNodeIsSecurityLabel(defel);
+	return false;
+}
+
+Oid pgaceParseSecurityLabel(DefElem *defel)
+{
+	if (sepgsqlIsEnabled() && defel)
+		return sepgsqlParseSecurityLabel(defel);
+	return InvalidOid;
+}
+
+/******************************************************************
+ * DATABASE related hooks
+ ******************************************************************/
+void pgaceSetDatabaseParam(const char *name, char *argstring)
+{
+	if (sepgsqlIsEnabled())
+		sepgsqlSetDatabaseParam(name, argstring);
+}
+
+void pgaceGetDatabaseParam(const char *name)
+{
+	if (sepgsqlIsEnabled())
+		sepgsqlGetDatabaseParam(name);
+}
+
+/******************************************************************
+ * FUNCTION related hooks
+ ******************************************************************/
+void pgaceCallFunction(FmgrInfo *finfo)
+{
+	if (sepgsqlIsEnabled())
+		sepgsqlCallFunction(finfo, false);
+}
+
+bool pgaceCallFunctionTrigger(FmgrInfo *finfo, TriggerData *tgdata)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlCallFunctionTrigger(finfo, tgdata);
+	return true;
+}
+
+void pgaceCallFunctionFastPath(FmgrInfo *finfo)
+{
+	if (sepgsqlIsEnabled())
+		sepgsqlCallFunction(finfo, true);
+}
+
+Datum pgacePreparePlanCheck(Relation rel)
+{
+	Oid saved = InvalidOid;
+
+	if (sepgsqlIsEnabled())
+		saved = sepgsqlPreparePlanCheck(rel);
+	return ObjectIdGetDatum(saved);
+}
+
+void pgaceRestorePlanCheck(Relation rel, Datum pgace_saved)
+{
+	if (sepgsqlIsEnabled())
+		sepgsqlRestorePlanCheck(rel, DatumGetObjectId(pgace_saved));
+}
+
+/******************************************************************
+ * TABLE related hooks
+ ******************************************************************/
+void pgaceLockTable(Oid relid)
+{
+	if (sepgsqlIsEnabled())
+		sepgsqlLockTable(relid);
+}
+
+bool pgaceAlterTable(Relation rel, AlterTableCmd *cmd)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlAlterTable(rel, cmd);
+	return false;
+}
+
+/******************************************************************
+ * COPY TO/COPY FROM statement hooks
+ ******************************************************************/
+void pgaceCopyTable(Relation rel, List *attNumList, bool isFrom) {
+	if (sepgsqlIsEnabled())
+		sepgsqlCopyTable(rel, attNumList, isFrom);
+}
+
+bool pgaceCopyToTuple(Relation rel, HeapTuple tuple) {
+	if (sepgsqlIsEnabled())
+		sepgsqlCopyToTuple(rel, tuple);
+	return true;
+}
+
+/******************************************************************
+ * Loadable shared library module hooks
+ ******************************************************************/
+void pgaceLoadSharedModule(const char *filename) {
+	if (sepgsqlIsEnabled())
+		sepgsqlLoadSharedModule(filename);
+}
+
+/******************************************************************
+ * Binary Large Object (BLOB) hooks
+ ******************************************************************/
+
+Oid pgaceLargeObjectGetSecurity(HeapTuple tuple) {
+	if (!sepgsqlIsEnabled())
+		selerror("SELinux is disabled");
+	return sepgsqlLargeObjectGetSecurity(tuple);
+}
+
+void pgaceLargeObjectSetSecurity(HeapTuple tuple, Oid lo_security, bool is_first) {
+	if (sepgsqlIsEnabled())
+		sepgsqlLargeObjectSetSecurity(tuple, lo_security, is_first);
+}
+
+void pgaceLargeObjectCreate(Relation rel, HeapTuple tuple) {
+	if (sepgsqlIsEnabled())
+		sepgsqlLargeObjectCreate(rel, tuple);
+}
+
+void pgaceLargeObjectDrop(Relation rel, HeapTuple tuple) {
+	if (sepgsqlIsEnabled())
+		sepgsqlLargeObjectDrop(rel, tuple);
+}
+
+void pgaceLargeObjectOpen(Relation rel, HeapTuple tuple, bool read_only) {
+	if (sepgsqlIsEnabled())
+		sepgsqlLargeObjectOpen(rel, tuple, read_only);
+}
+
+void pgaceLargeObjectRead(Relation rel, HeapTuple tuple, bool is_first) {
+	if (sepgsqlIsEnabled())
+		sepgsqlLargeObjectRead(rel, tuple);
+}
+
+void pgaceLargeObjectWrite(Relation rel, HeapTuple newtup, HeapTuple oldtup, bool is_first) {
+	if (sepgsqlIsEnabled())
+		sepgsqlLargeObjectWrite(rel, newtup, oldtup);
+}
+
+void pgaceLargeObjectTruncate(Relation rel, Oid loid) {
+	if (sepgsqlIsEnabled())
+		sepgsqlLargeObjectTruncate(rel, loid);
+}
+
+void pgaceLargeObjectImport(int fd) {
+	if (sepgsqlIsEnabled())
+		sepgsqlLargeObjectImport();
+}
+
+void pgaceLargeObjectExport(int fd, Oid loid) {
+	if (sepgsqlIsEnabled())
+		sepgsqlLargeObjectExport();
+}
+
+/******************************************************************
+ * Security Label hooks
+ ******************************************************************/
+char *pgaceSecurityLabelIn(char *seclabel)
+{
+	if (sepgsqlIsEnabled())
+		seclabel = sepgsqlSecurityLabelIn(seclabel);
+	return seclabel;
+}
+
+char *pgaceSecurityLabelOut(char *seclabel)
+{
+	if (sepgsqlIsEnabled())
+		seclabel = sepgsqlSecurityLabelOut(seclabel);
+	return seclabel;
+}
+
+bool pgaceSecurityLabelIsValid(char *seclabel)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlSecurityLabelIsValid(seclabel);
+	return true;
+}
+
+char *pgaceSecurityLabelOfLabel(char *new_label)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlSecurityLabelOfLabel(new_label);
+	return pstrdup("unlabeled");
+}
+
+char *pgaceSecurityLabelNotFound(Oid sid)
+{
+	if (sepgsqlIsEnabled())
+		sepgsqlSecurityLabelNotFound(sid);
+	return pstrdup("unlabeled");
+}
+
+/******************************************************************
+ * Extended node type hooks
+ ******************************************************************/
+Node *pgaceCopyObject(Node *orig)
+{
+	if (sepgsqlIsEnabled())
+		return sepgsqlCopyObject(orig);
+	return NULL;
+}
+
+bool pgaceOutObject(StringInfo str, Node *node)
+{
+	if (sepgsqlIsEnabled())
+		sepgsqlOutObject(str, node);
+	return false;
+}
