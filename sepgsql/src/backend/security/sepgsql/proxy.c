9bb5aaf55ee00278c5c7deb10f6b5d66ef7b26f4
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-03-02 08:00:56 +0000

    BUGFIX: SELECT count(*) does not filter unallowed tuples

--- sepgsql/src/backend/security/sepgsql/proxy.c
+++ sepgsql/src/backend/security/sepgsql/proxy.c
@@ -398,6 +398,34 @@ static List *walkOpExprHelper(List *selist, Oid opid)
 	return selist;
 }
 
+static List *walkAggrefHelper(List *selist, Query *query, Node *node)
+{
+	if (node == NULL)
+		return selist;
+
+	if (IsA(node, RangeTblRef)) {
+		RangeTblRef *rtr = (RangeTblRef *) node;
+		RangeTblEntry *rte = rt_fetch(rtr->rtindex, query->rtable);
+
+		if (rte->rtekind == RTE_RELATION)
+			selist = addEvalPgAttribute(selist, rte, 0, DB_COLUMN__SELECT);
+	} else if (IsA(node, JoinExpr)) {
+		JoinExpr *j = (JoinExpr *) node;
+
+		selist = walkAggrefHelper(selist, query, j->larg);
+		selist = walkAggrefHelper(selist, query, j->rarg);
+	} else if (IsA(node, FromExpr)) {
+		FromExpr *fm = (FromExpr *)node;
+		ListCell *l;
+
+		foreach (l, fm->fromlist)
+			selist = walkAggrefHelper(selist, query, lfirst(l));
+	} else {
+		elog(ERROR, "SELinux: unexpected node type (%d) at Query->fromlist", nodeTag(node));
+	}
+	return selist;
+}
+
 static List *sepgsqlWalkExpr(List *selist, queryChain *qc, Node *node, int flags)
 {
 	if (node == NULL)
@@ -435,6 +463,10 @@ static List *sepgsqlWalkExpr(List *selist, queryChain *qc, Node *node, int flags
 
 		selist = addEvalPgProc(selist, aggref->aggfnoid, DB_PROCEDURE__EXECUTE);
 		selist = sepgsqlWalkExpr(selist, qc, (Node *) aggref->args, flags);
+		if (aggref->aggstar) {
+			Query *query = getQueryFromChain(qc);
+			selist = walkAggrefHelper(selist, query, (Node *) query->jointree);
+		}
 		break;
 	}
 	case T_OpExpr:
