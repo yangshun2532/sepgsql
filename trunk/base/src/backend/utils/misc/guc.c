0c249029b088e83247557db4c2fa44626b86d0b7
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-03-19 01:33:12 +0000

    CVS pull -r HEAD at Wed Mar 19 10:22:56 JST 2008

--- trunk/base/src/backend/utils/misc/guc.c
+++ trunk/base/src/backend/utils/misc/guc.c
@@ -10,7 +10,7 @@
  * Written by Peter Eisentraut <peter_e@gmx.net>.
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/misc/guc.c,v 1.437 2008/03/10 12:55:13 mha Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/misc/guc.c,v 1.439 2008/03/17 17:45:09 mha Exp $
  *
  *--------------------------------------------------------------------
  */
@@ -167,6 +167,11 @@ static const char *show_tcp_keepalives_interval(void);
 static const char *show_tcp_keepalives_count(void);
 static bool assign_autovacuum_max_workers(int newval, bool doit, GucSource source);
 static bool assign_maxconnections(int newval, bool doit, GucSource source);
+
+static char *config_enum_get_options(struct config_enum *record, 
+									 const char *prefix, const char *suffix);
+
+
 
 /*
  * Options for enum values defined in this module.
@@ -3134,8 +3139,9 @@ InitializeGUCOptions(void)
 					if (conf->assign_hook)
 						if (!(*conf->assign_hook) (conf->boot_val, true,
 												   PGC_S_DEFAULT))
-							elog(FATAL, "failed to initialize %s to %d",
-								 conf->gen.name, conf->boot_val);
+							elog(FATAL, "failed to initialize %s to %s",
+								 conf->gen.name, 
+								 config_enum_lookup_by_value(conf, conf->boot_val));
 					*conf->variable = conf->reset_val = conf->boot_val;
 					break;
 				}
@@ -4210,8 +4216,8 @@ parse_real(const char *value, double *result)
  * The returned string is a pointer to static data and not
  * allocated for modification.
  */
-static const char *
-config_enum_lookup_value(struct config_enum *record, int val)
+const char *
+config_enum_lookup_by_value(struct config_enum *record, int val)
 {
 	const struct config_enum_entry *entry = record->options;
 	while (entry && entry->name)
@@ -4230,11 +4236,11 @@ config_enum_lookup_value(struct config_enum *record, int val)
  * Lookup the value for an enum option with the selected name
  * (case-insensitive).
  * If the enum option is found, sets the retval value and returns
- * true. If it's not found, return FALSE and don't touch retval.
+ * true. If it's not found, return FALSE and retval is set to 0.
  *
  */
-static bool
-config_enum_lookup_name(struct config_enum *record, const char *value, int *retval)
+bool
+config_enum_lookup_by_name(struct config_enum *record, const char *value, int *retval)
 {
 	const struct config_enum_entry *entry = record->options;
 	
@@ -4243,7 +4249,7 @@ config_enum_lookup_name(struct config_enum *record, const char *value, int *retv
 	
 	while (entry && entry->name)
 	{
-		if (!pg_strcasecmp(value, entry->name))
+		if (pg_strcasecmp(value, entry->name) == 0)
 		{
 			*retval = entry->val;
 			return TRUE;
@@ -4255,10 +4261,10 @@ config_enum_lookup_name(struct config_enum *record, const char *value, int *retv
 
 
 /*
- * Returna list of all available options for an enum, separated
+ * Return a list of all available options for an enum, separated
  * by ", " (comma-space).
- * If prefix is gievn, it is added before the first enum value.
- * If suffix is given, it is added to the end of the string.
+ * If prefix is non-NULL, it is added before the first enum value.
+ * If suffix is non-NULL, it is added to the end of the string.
  */
 static char *
 config_enum_get_options(struct config_enum *record, const char *prefix, const char *suffix)
@@ -4867,7 +4873,7 @@ set_config_option(const char *name, const char *value,
 
 				if (value)
 				{
-					if (!config_enum_lookup_name(conf, value, &newval))
+					if (!config_enum_lookup_by_name(conf, value, &newval))
 					{
 						char *hintmsg = config_enum_get_options(conf, "Available values: ", ".");
 
@@ -4895,8 +4901,9 @@ set_config_option(const char *name, const char *value,
 					{
 						ereport(elevel,
 								(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-								 errmsg("invalid value for parameter \"%s\": \"%d\"",
-										name, newval)));
+								 errmsg("invalid value for parameter \"%s\": \"%s\"",
+										name, 
+										config_enum_lookup_by_value(conf, newval))));
 						return false;
 					}
 
@@ -4997,7 +5004,7 @@ GetConfigOption(const char *name)
 			return *((struct config_string *) record)->variable;
 
 		case PGC_ENUM:
-			return config_enum_lookup_value((struct config_enum *) record,
+			return config_enum_lookup_by_value((struct config_enum *) record,
 											*((struct config_enum *) record)->variable);
 	}
 	return NULL;
@@ -5045,7 +5052,7 @@ GetConfigOptionResetString(const char *name)
 			return ((struct config_string *) record)->reset_val;
 
 		case PGC_ENUM:
-			return config_enum_lookup_value((struct config_enum *) record,
+			return config_enum_lookup_by_value((struct config_enum *) record,
 										    ((struct config_enum *) record)->reset_val);
 	}
 	return NULL;
@@ -5587,6 +5594,30 @@ DefineCustomStringVariable(const char *name,
 	/* we could probably do without strdup, but keep it like normal case */
 	if (var->boot_val)
 		var->reset_val = guc_strdup(ERROR, var->boot_val);
+	var->assign_hook = assign_hook;
+	var->show_hook = show_hook;
+	define_custom_variable(&var->gen);
+}
+
+void
+DefineCustomEnumVariable(const char *name,
+						 const char *short_desc,
+						 const char *long_desc,
+						 int *valueAddr,
+						 const struct config_enum_entry *options,
+						 GucContext context,
+						 GucEnumAssignHook assign_hook,
+						 GucShowHook show_hook)
+{
+	struct config_enum *var;
+
+	var = (struct config_enum *)
+		init_custom_variable(name, short_desc, long_desc, context,
+							 PGC_ENUM, sizeof(struct config_enum));
+	var->variable = valueAddr;
+	var->boot_val = *valueAddr;
+	var->reset_val = *valueAddr;
+	var->options = options;
 	var->assign_hook = assign_hook;
 	var->show_hook = show_hook;
 	define_custom_variable(&var->gen);
@@ -6231,7 +6262,7 @@ _ShowOption(struct config_generic * record, bool use_units)
 				if(conf->show_hook)
 					val = (*conf->show_hook) ();
 				else
-					val = config_enum_lookup_value(conf, *conf->variable);
+					val = config_enum_lookup_by_value(conf, *conf->variable);
 			}
 			break;
 
@@ -6297,7 +6328,7 @@ is_newvalue_equal(struct config_generic * record, const char *newvalue)
 				struct config_enum *conf = (struct config_enum *) record;
 				int			newval;
 
-				return config_enum_lookup_name(conf, newvalue, &newval)
+				return config_enum_lookup_by_name(conf, newvalue, &newval)
 					&& *conf->variable == newval;
 			}
 	}
@@ -6391,7 +6422,7 @@ write_nondefault_variables(GucContext context)
 					{
 						struct config_enum *conf = (struct config_enum *) gconf;
 						
-						fprintf(fp, "%s", config_enum_lookup_value(conf, *conf->variable));
+						fprintf(fp, "%s", config_enum_lookup_by_value(conf, *conf->variable));
 					}
 					break;
 			}
