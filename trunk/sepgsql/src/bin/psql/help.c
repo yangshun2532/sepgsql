65ebeefc57e48d0577898e06682ffaa372f35501
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-04-08 07:47:30 +0000

    merge updates of /trunk/pgace into /trunk/sepgsql at Tue Apr  8 16:27:54 JST 2008

--- trunk/sepgsql/src/bin/psql/help.c
+++ trunk/sepgsql/src/bin/psql/help.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2000-2008, PostgreSQL Global Development Group
  *
- * $PostgreSQL: pgsql/src/bin/psql/help.c,v 1.124 2008/03/29 19:40:12 tgl Exp $
+ * $PostgreSQL: pgsql/src/bin/psql/help.c,v 1.126 2008/04/04 18:00:25 momjian Exp $
  */
 #include "postgres_fe.h"
 
