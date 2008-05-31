6e697bc7fcbd3ba9887418cd218232328d3ba67f
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-05-31 06:50:56 +0000

    new style core.c

--- trunk/sepgsql/src/backend/security/sepgsql/core.c
+++ trunk/sepgsql/src/backend/security/sepgsql/core.c
@@ -6,187 +6,168 @@
  */
 #include "postgres.h"
 
+#include "catalog/pg_database.h"
+#include "catalog/pg_security.h"
+#include "libpq/libpq.h"
+#include "miscadmin.h"
 #include "security/pgace.h"
+#include "utils/syscache.h"
 
-/* sepgsql_getcon() -- returns a security context of client */
-Datum
-sepgsql_getcon(PG_FUNCTION_ARGS)
+static security_context_t serverContext = NULL;
+static security_context_t clientContext = NULL;
+static security_context_t databaseContext = NULL;
+
+const security_context_t sepgsqlGetServerContext(void)
 {
-	PG_RETURN_OID(sepgsqlGetClientContext());
+	Assert(serverContext != NULL);
+	return serverContext;
 }
 
-/* sepgsql_system_getcon() -- obtain the server's context */
-static Oid sepgsql_system_getcon()
+const security_context_t sepgsqlGetClientContext(void)
 {
+	Assert(clientContext != NULL);
+	return clientContext;
+}
+
+const security_context_t sepgsqlGetDatabaseContext(void)
+{
+	Assert(databaseContext != NULL);
+	return databaseContext;
+}
+
+const security_context_t sepgsqlGetDefaultDatabaseContext(void)
+{
+	static security_context_t defaultDatabaseContext = NULL;
 	security_context_t context;
-	Oid ssid;
-
-	if (getcon_raw(&context) != 0)
-		elog(ERROR, "SELinux: could not obtain security context of server process");
-
-	PG_TRY();
-	{
-		ssid = DatumGetObjectId(DirectFunctionCall1(security_label_raw_in,
-													CStringGetDatum(context)));
-	}
-	PG_CATCH();
-	{
-		freecon(context);
-		PG_RE_THROW();
-	}
-	PG_END_TRY();
-	freecon(context);
-	return ssid;
-}
-
-/* sepgsql_system_getpeercon() -- obtain the client's context */
-static Oid sepgsql_system_getpeercon(int sockfd)
-{
-	security_context_t context, __context;
-	Oid ssid;
-
-	if (getpeercon_raw(sockfd, &context)) {
-		/* we can set finally fallbacked context */
-		__context = getenv("SEPGSQL_FALLBACK_CONTEXT");
-		if (!__context)
-			elog(ERROR, "SELinux: could not obtain security context of database client");
-		if (security_check_context(__context) ||
-			selinux_trans_to_raw_context(__context, &context))
-			elog(ERROR, "SELinux: '%s' is not a valid context", __context);
-	}
-
-	PG_TRY();
-	{
-		ssid = DatumGetObjectId(DirectFunctionCall1(security_label_raw_in,
-													CStringGetDatum(context)));
-	}
-	PG_CATCH();
-	{
-		freecon(context);
-		PG_RE_THROW();
-	}
-	PG_END_TRY();
-	freecon(context);
-	return ssid;
-}
-
-static Oid sepgsqlServerContext = InvalidOid;
-static Oid sepgsqlClientContext = InvalidOid;
-
-Oid sepgsqlGetServerContext()
-{
-	return sepgsqlServerContext;
-}
-
-Oid sepgsqlGetClientContext()
-{
-	return sepgsqlClientContext;
-}
-
-void sepgsqlSetClientContext(Oid new_context)
-{
-	sepgsqlClientContext = new_context;
-}
-
-Oid sepgsqlGetDefaultDatabaseContext(void)
-{
-	static Oid default_dbcon_cached = InvalidOid;
-	char *context, *user, *role, *type, *range;
+	char *user, *role, *type, *range;
 	char buffer[1024];
 
-	if (default_dbcon_cached != InvalidOid)
-		return default_dbcon_cached;
+	if (defaultDatabaseContext)
+		return defaultDatabaseContext;
 
 	/*
-	 * TODO: we should call selabel_lookup() here, when libselinux
-	 *       got SE-PostgreSQL default database context support.
+	 * TODO: we should call selabel_lookup() here, when libselinux got
+	 *       default database context support.
 	 */
-	context = DatumGetCString(DirectFunctionCall1(security_label_raw_out,
-								 ObjectIdGetDatum(sepgsqlGetClientContext())));
+	context = pstrdup(sepgsqlGetClientContext());
+
 	user = strtok(context, ":");
 	role = strtok(NULL, ":");
 	type = strtok(NULL, ":");
 	range = strtok(NULL, "-");
-	snprintf(buffer, sizeof(buffer), "%s:%s:%s:%s",
-			 user, "object_r", "sepgsql_db_t", range);
-	default_dbcon_cached =
-		DatumGetObjectId(DirectFunctionCall1(security_label_raw_in,
-											 CStringGetDatum(buffer)));
 
-	return default_dbcon_cached;
+	snprintf(buffer, sizeof(buffer), "%s:object_r:%s:%s",
+			 user, "sepgsql_db_t", range);
+	if (security_check_context_raw((security_context_t) buffer))
+		elog(ERROR, "SELinux: invalid default database context");
+
+	defaultDatabaseContext = strdup(buffer);
+	if (!defaultDatabaseContext)
+		elog(ERROR, "SELinux: memory allocation error");
+
+	return defaultDatabaseContext;
 }
 
-Oid sepgsqlGetDatabaseContext()
+static void initContexts(void)
 {
-	HeapTuple tuple;
-	Oid datcon;
+	/* server context */
+	if (getcon_raw(&serverContext))
+		elog(ERROR, "SELinux: could not get security context of server process");
 
-	if (IsBootstrapProcessingMode())
-		return sepgsqlGetDefaultDatabaseContext();
+	/* client context */
+	if (!MyProcPort)
+	{
+		/* a client process is a server process in same time */
+		clientContext = serverContext;
+	}
+	else
+	{
+		if (getpeercon_raw(MyProcPort->sock, &clientContext))
+		{
+			/* fallbacked security context */
+			char *fallback = getenv("SEPGSQL_FALLBACK_CONTEXT");
 
-	tuple = SearchSysCache(DATABASEOID,
-						   ObjectIdGetDatum(MyDatabaseId),
-						   0, 0, 0);
-	if (!HeapTupleIsValid(tuple))
-		elog(ERROR, "SELinux: cache lookup failed for database %u", MyDatabaseId);
-	datcon = HeapTupleGetSecurity(tuple);
-	ReleaseSysCache(tuple);
-
-	return datcon;
-}
-
-char *sepgsqlGetDatabaseName()
-{
-	Form_pg_database dat_form;
-	HeapTuple tuple;
-	char *datname;
-
-	if (IsBootstrapProcessingMode())
-		return NULL;
-
-	tuple = SearchSysCache(DATABASEOID,
-						   ObjectIdGetDatum(MyDatabaseId),
-						   0, 0, 0);
-	if (!HeapTupleIsValid(tuple))
-		elog(ERROR, "SELinux: cache lookup failed for database %u", MyDatabaseId);
-	dat_form = (Form_pg_database) GETSTRUCT(tuple);
-	datname = pstrdup(NameStr(dat_form->datname));
-	ReleaseSysCache(tuple);
-
-	return datname;
-}
-
-void sepgsqlInitialize(bool is_bootstrap)
-{
-	sepgsql_avc_init();
-
-	/* obtain security context of server process */
-	sepgsqlServerContext = sepgsql_system_getcon();
-
-	if (IsBootstrapProcessingMode()) {
-		sepgsqlClientContext = sepgsqlServerContext;
-		sepgsql_avc_permission(sepgsqlGetClientContext(),
-							   sepgsqlGetDatabaseContext(),
-							   SECCLASS_DB_DATABASE,
-							   DB_DATABASE__ACCESS,
-							   NULL);
-		return;
+			if (!fallback)
+				elog(ERROR, "SELinux: could not get security context of client process");
+			if (security_check_context(fallback)
+				|| selinux_trans_to_raw_context(fallback, &clientContext))
+				elog(ERROR, "SELinux: %s is not a valid security context", fallback);
+		}
 	}
 
-	/* obtain security context of client process */
-	if (MyProcPort != NULL)
-		sepgsqlClientContext = sepgsql_system_getpeercon(MyProcPort->sock);
+	/* database context */
+	if (IsBootstrapProcessingMode())
+	{
+		databaseContext = sepgsqlGetDefaultDatabaseContext();
+	}
 	else
-		sepgsqlClientContext = sepgsql_system_getcon();
+	{
+		HeapTuple tuple;
+		Oid security_id;
+		Datum labelTxt;
+		bool isnull;
 
-	sepgsql_avc_permission(sepgsqlGetClientContext(),
-						   sepgsqlGetDatabaseContext(),
-						   SECCLASS_DB_DATABASE,
-						   DB_DATABASE__ACCESS,
-						   sepgsqlGetDatabaseName());
+		tuple = SearchSysCache(DATABASEOID,
+							   ObjectIdGetDatum(MyDatabaseId),
+							   0, 0, 0);
+		if (!HeapTupleIsValid(tuple))
+			elog(ERROR, "SELinux: cache lookup failed for database %u", MyDatabaseId);
+		security_id = HeapTupleGetSecurity(tuple);
+		ReleaseSysCache(tuple);
+
+		tuple = SearchSysCache(SECURITYOID,
+							   ObjectIdGetDatum(security_id),
+							   0, 0, 0);
+		if (!HeapTupleIsValid(tuple))
+			elog(ERROR, "SELinux: cache lookup failed for security id %u", security_id);
+		labelTxt = SysCacheGetAttr(SECURITYOID,
+								   tuple,
+								   Anum_pg_security_seclabel,
+								   &isnull);
+		Assert(isnull != false);
+
+		databaseContext = strdup(TextDatumGetCString(labelTxt));
+		if (!databaseContext)
+			elog(ERROR, "SELinux: memory allocation error");
+	}
 }
 
-bool sepgsqlIsEnabled()
+void sepgsqlInitialize(bool bootstrap)
+{
+	char *dbname;
+
+	sepgsqlAvcInit();
+
+	initContexts();
+
+	/* check db_database:{ access } */
+	if (IsBootstrapProcessingMode())
+		dbname = "template1";
+	else
+	{
+		Form_pg_database dbForm;
+		HeapTuple tuple;
+
+		tuple = SearchSysCache(DATABASEOID,
+							   ObjectIdGetDatum(MyDatabaseId),
+							   0, 0, 0);
+		if (!HeapTupleIsValid(tuple))
+			elog(ERROR, "SELinux: cache lookup failed for database %u", MyDatabaseId);
+		dbForm = (Form_pg_database) GETSTRUCT(tuple);
+
+		dbname = pstrdup(NameStr(dbForm->datname));
+
+		ReleaseSysCache(tuple);
+	}
+
+	sepgsqlAvcPermission(sepgsqlGetClientContext(),
+						 sepgsqlGetDatabaseContext(),
+						 SECCLASS_DB_DATABASE,
+						 DB_DATABASE__ACCESS,
+						 dbname);
+}
+
+bool sepgsqlIsEnabled(void)
 {
 	static int enabled = -1;
 
@@ -195,3 +176,54 @@ bool sepgsqlIsEnabled()
 
 	return enabled > 0 ? true : false;
 }
+
+/*
+ * sepgsql_getcon(void) -- returns a security context of client
+ */
+Datum sepgsql_getcon(PG_FUNCTION_ARGS)
+{
+	security_context_t context;
+	Datum labelTxt;
+
+	if (selinux_raw_to_trans_context(clientContext, &context))
+		elog(ERROR, "SELinux: could not translate mls label");
+	PG_TRY();
+	{
+		labelTxt = CStringGetTextDatum(context);
+	}
+	PG_CATCH();
+	{
+		freecon(context);
+		PG_RE_THROW();
+	}
+	PG_END_TRY();
+	freecon(context);
+
+	return labelTxt;
+}
+
+#if 0
+Datum sepgsql_get_user(PG_FUNCTION_ARGS)
+{}
+
+Datum sepgsql_set_user(PG_FUNCTION_ARGS)
+{}
+
+Datum sepgsql_get_role(PG_FUNCTION_ARGS)
+{}
+
+Datum sepgsql_set_role(PG_FUNCTION_ARGS)
+{}
+
+Datum sepgsql_get_type(PG_FUNCTION_ARGS)
+{}
+
+Datum sepgsql_set_type(PG_FUNCTION_ARGS)
+{}
+
+Datum sepgsql_get_range(PG_FUNCTION_ARGS)
+{}
+
+Datum sepgsql_set_range(PG_FUNCTION_ARGS)
+{}
+#endif
