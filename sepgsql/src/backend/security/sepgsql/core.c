008ddaf63b12c3c157dcfcfb6d81980d30a26dfc
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-06-02 11:45:13 +0000

    add sepgsql_(get|set)_(user|role|type|range) functions

--- sepgsql/src/backend/security/sepgsql/core.c
+++ sepgsql/src/backend/security/sepgsql/core.c
@@ -12,6 +12,7 @@
 #include "miscadmin.h"
 #include "security/pgace.h"
 #include "utils/syscache.h"
+#include <selinux/context.h>
 
 static security_context_t serverContext = NULL;
 static security_context_t clientContext = NULL;
@@ -194,31 +195,233 @@ Datum sepgsql_getcon(PG_FUNCTION_ARGS)
 	PG_END_TRY();
 	freecon(context);
 
-	return labelTxt;
+	PG_RETURN_DATUM(labelTxt);
 }
 
-#if 0
+Datum sepgsql_getservcon(PG_FUNCTION_ARGS)
+{
+	security_context_t context;
+	Datum labelTxt;
+
+	if (selinux_raw_to_trans_context(serverContext, &context))
+		elog(ERROR, "SELinux: could not translate mls label");
+	PG_TRY();
+	{
+		labelTxt = CStringGetTextDatum(context);
+	}
+	PG_CATCH();
+	{
+		freecon(context);
+		PG_RE_THROW();
+	}
+	PG_END_TRY();
+	freecon(context);
+
+	PG_RETURN_DATUM(labelTxt);
+}
+
+static void parse_to_context(security_context_t context,
+							 char **user, char **role, char **type, char **range)
+{
+	security_context_t raw_context;
+
+	if (selinux_trans_to_raw_context(context, &raw_context) < 0)
+		elog(ERROR, "SELinux: could not parse given security context");
+	PG_TRY();
+	{
+		char *tmp;
+
+		tmp = pstrdup(strtok(raw_context, ":"));
+		if (user)
+			*user = tmp;
+		tmp = pstrdup(strtok(NULL, ":"));
+		if (role)
+			*role = tmp;
+		tmp = pstrdup(strtok(NULL, ":"));
+		if (type)
+			*type = tmp;
+		if (is_selinux_mls_enabled())
+		{
+			tmp = pstrdup(strtok(NULL, "\0"));
+			if (range)
+				*range = tmp;
+		}
+		else if (range)
+			*range = NULL;
+	}
+	PG_CATCH();
+	{
+		freecon(raw_context);
+		PG_RE_THROW();
+	}
+	PG_END_TRY();
+    freecon(raw_context);
+}
+
 Datum sepgsql_get_user(PG_FUNCTION_ARGS)
-{}
+{
+	char *user;
+
+	parse_to_context(TextDatumGetCString(PG_GETARG_TEXT_P(0)),
+					 &user, NULL, NULL, NULL);
+	PG_RETURN_TEXT_P(CStringGetTextDatum(user));
+}
 
 Datum sepgsql_set_user(PG_FUNCTION_ARGS)
-{}
+{
+	char *user, *role, *type, *range;
+	char buffer[1024];
+	security_context_t newcon;
+	Datum result;
+
+	parse_to_context(TextDatumGetCString(PG_GETARG_TEXT_P(0)),
+					 &user, &role, &type, &range);
+	if (range)
+		snprintf(buffer, sizeof(buffer), "%s:%s:%s:%s",
+				 TextDatumGetCString(PG_GETARG_TEXT_P(1)), role, type, range);
+	else
+		snprintf(buffer, sizeof(buffer), "%s:%s:%s",
+				 TextDatumGetCString(PG_GETARG_TEXT_P(1)), role, type);
+	if (selinux_raw_to_trans_context((security_context_t) buffer, &newcon) < 0)
+		elog(ERROR, "SELinux: could not set a new range");
+
+	PG_TRY();
+    {
+		result = CStringGetTextDatum(newcon);
+	}
+    PG_CATCH();
+    {
+		freecon(newcon);
+        PG_RE_THROW();
+    }
+    PG_END_TRY();
+	freecon(newcon);
+
+	PG_RETURN_DATUM(result);
+}
 
 Datum sepgsql_get_role(PG_FUNCTION_ARGS)
-{}
+{
+	char *role;
+
+	parse_to_context(TextDatumGetCString(PG_GETARG_TEXT_P(0)),
+					 NULL, &role, NULL, NULL);
+	PG_RETURN_TEXT_P(CStringGetTextDatum(role));
+}
 
 Datum sepgsql_set_role(PG_FUNCTION_ARGS)
-{}
+{
+	char *user, *role, *type, *range;
+	char buffer[1024];
+	security_context_t newcon;
+	Datum result;
+
+	parse_to_context(TextDatumGetCString(PG_GETARG_TEXT_P(0)),
+					 &user, &role, &type, &range);
+	if (range)
+		snprintf(buffer, sizeof(buffer), "%s:%s:%s:%s",
+				 user, TextDatumGetCString(PG_GETARG_TEXT_P(1)), type, range);
+	else
+		snprintf(buffer, sizeof(buffer), "%s:%s:%s:%s",
+				 user, TextDatumGetCString(PG_GETARG_TEXT_P(1)), type);
+	if (selinux_raw_to_trans_context((security_context_t) buffer, &newcon) < 0)
+		elog(ERROR, "SELinux: could not set a new range");
+
+	PG_TRY();
+    {
+		result = CStringGetTextDatum(newcon);
+	}
+    PG_CATCH();
+    {
+		freecon(newcon);
+        PG_RE_THROW();
+    }
+    PG_END_TRY();
+	freecon(newcon);
+
+	PG_RETURN_DATUM(result);
+}
 
 Datum sepgsql_get_type(PG_FUNCTION_ARGS)
-{}
+{
+	char *type;
+
+	parse_to_context(TextDatumGetCString(PG_GETARG_TEXT_P(0)),
+					 NULL, NULL, &type, NULL);
+	PG_RETURN_TEXT_P(CStringGetTextDatum(type));
+}
 
 Datum sepgsql_set_type(PG_FUNCTION_ARGS)
-{}
+{
+	char *user, *role, *type, *range;
+	char buffer[1024];
+	security_context_t newcon;
+	Datum result;
+
+	parse_to_context(TextDatumGetCString(PG_GETARG_TEXT_P(0)),
+					 &user, &role, &type, &range);
+	if (range)
+		snprintf(buffer, sizeof(buffer), "%s:%s:%s:%s",
+				 user, role, TextDatumGetCString(PG_GETARG_TEXT_P(1)), range);
+	else
+		snprintf(buffer, sizeof(buffer), "%s:%s:%s",
+				 user, role, TextDatumGetCString(PG_GETARG_TEXT_P(1)));
+	if (selinux_raw_to_trans_context((security_context_t) buffer, &newcon) < 0)
+		elog(ERROR, "SELinux: could not set a new range");
+
+	PG_TRY();
+    {
+		result = CStringGetTextDatum(newcon);
+	}
+    PG_CATCH();
+    {
+		freecon(newcon);
+        PG_RE_THROW();
+    }
+    PG_END_TRY();
+	freecon(newcon);
+
+	PG_RETURN_DATUM(result);
+}
 
 Datum sepgsql_get_range(PG_FUNCTION_ARGS)
-{}
+{
+	char *range;
+
+	parse_to_context(TextDatumGetCString(PG_GETARG_TEXT_P(0)),
+					 NULL, NULL, NULL, &range);
+	PG_RETURN_TEXT_P(CStringGetTextDatum(range));
+}
 
 Datum sepgsql_set_range(PG_FUNCTION_ARGS)
-{}
-#endif
+{
+	char *user, *role, *type, *range;
+	char buffer[1024];
+	security_context_t newcon;
+	Datum result;
+
+	parse_to_context(TextDatumGetCString(PG_GETARG_TEXT_P(0)),
+					 &user, &role, &type, &range);
+	if (range)
+		snprintf(buffer, sizeof(buffer), "%s:%s:%s:%s",
+				 user, role, type,TextDatumGetCString(PG_GETARG_TEXT_P(1)));
+	else
+		snprintf(buffer, sizeof(buffer), "%s:%s:%s",
+				 user, role, type);
+	if (selinux_raw_to_trans_context((security_context_t) buffer, &newcon) < 0)
+		elog(ERROR, "SELinux: could not set a new range");
+
+	PG_TRY();
+    {
+		result = CStringGetTextDatum(newcon);
+	}
+    PG_CATCH();
+    {
+		freecon(newcon);
+        PG_RE_THROW();
+    }
+    PG_END_TRY();
+	freecon(newcon);
+
+	PG_RETURN_DATUM(result);
+}
