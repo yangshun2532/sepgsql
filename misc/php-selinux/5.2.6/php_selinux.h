7304bae7840496fa0a3b4add9b861c1efa89eaf2
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-09-06 13:15:30 +0000

    add libselinux functions
    TODO: obtain file/socket descriptor

--- misc/php-selinux/5.2.6/php_selinux.h
+++ misc/php-selinux/5.2.6/php_selinux.h
@@ -10,6 +10,55 @@ PHP_MINIT_FUNCTION(selinux);
 PHP_FUNCTION(selinux_is_enabled);
 PHP_FUNCTION(selinux_mls_is_enabled);
 PHP_FUNCTION(selinux_getcon);
+PHP_FUNCTION(selinux_getcon_raw);
+PHP_FUNCTION(selinux_setcon);
+PHP_FUNCTION(selinux_setcon_raw);
+PHP_FUNCTION(selinux_getpidcon);
+PHP_FUNCTION(selinux_getpidcon_raw);
+PHP_FUNCTION(selinux_getprevcon);
+PHP_FUNCTION(selinux_getprevcon_raw);
+PHP_FUNCTION(selinux_getexeccon);
+PHP_FUNCTION(selinux_getexeccon_raw);
+PHP_FUNCTION(selinux_setexeccon);
+PHP_FUNCTION(selinux_setexeccon_raw);
+PHP_FUNCTION(selinux_getfscreatecon);
+PHP_FUNCTION(selinux_getfscreatecon_raw);
+PHP_FUNCTION(selinux_setfscreatecon);
+PHP_FUNCTION(selinux_setfscreatecon_raw);
+PHP_FUNCTION(selinux_getkeycreatecon);
+PHP_FUNCTION(selinux_getkeycreatecon_raw);
+PHP_FUNCTION(selinux_setkeycreatecon);
+PHP_FUNCTION(selinux_setkeycreatecon_raw);
+PHP_FUNCTION(selinux_getsockcreatecon);
+PHP_FUNCTION(selinux_getsockcreatecon_raw);
+PHP_FUNCTION(selinux_setsockcreatecon);
+PHP_FUNCTION(selinux_setsockcreatecon_raw);
+
+/*
+ * Get file context
+ */
+PHP_FUNCTION(selinux_getfilecon);
+PHP_FUNCTION(selinux_getfilecon_raw);
+PHP_FUNCTION(selinux_lgetfilecon);
+PHP_FUNCTION(selinux_lgetfilecon_raw);
+PHP_FUNCTION(selinux_fgetfilecon);
+PHP_FUNCTION(selinux_fgetfilecon_raw);
+
+/*
+ * Set file context
+ */
+PHP_FUNCTION(selinux_setfilecon);
+PHP_FUNCTION(selinux_setfilecon_raw);
+PHP_FUNCTION(selinux_lsetfilecon);
+PHP_FUNCTION(selinux_lsetfilecon_raw);
+PHP_FUNCTION(selinux_fsetfilecon);
+PHP_FUNCTION(selinux_fsetfilecon_raw);
+
+/*
+ * Labeled Networking
+ */
+PHP_FUNCTION(selinux_getpeercon);
+PHP_FUNCTION(selinux_getpeercon_raw);
 
 #else	/* HAVE_SELINUX */
 
--- misc/php-selinux/5.2.6/selinux.c
+++ misc/php-selinux/5.2.6/selinux.c
@@ -18,9 +18,54 @@
  * SELinux functions
  */
 zend_function_entry selinux_functions[] = {
-	PHP_FE(selinux_is_enabled,	NULL)
-	PHP_FE(selinux_mls_is_enabled,	NULL)
-	PHP_FE(selinux_getcon,		NULL)
+	/*  wrappers for the /proc/pid/attr API */
+	PHP_FE(selinux_is_enabled,		NULL)
+	PHP_FE(selinux_mls_is_enabled,		NULL)
+	PHP_FE(selinux_getcon,			NULL)
+	PHP_FE(selinux_getcon_raw,		NULL)
+	PHP_FE(selinux_setcon,			NULL)
+	PHP_FE(selinux_setcon_raw,		NULL)
+	PHP_FE(selinux_getpidcon,		NULL)
+	PHP_FE(selinux_getpidcon_raw,		NULL)
+	PHP_FE(selinux_getprevcon,		NULL)
+	PHP_FE(selinux_getexeccon,		NULL)
+	PHP_FE(selinux_getexeccon_raw,		NULL)
+	PHP_FE(selinux_setexeccon,		NULL)
+	PHP_FE(selinux_setexeccon_raw,		NULL)
+	PHP_FE(selinux_getfscreatecon,		NULL)
+	PHP_FE(selinux_getfscreatecon_raw,	NULL)
+	PHP_FE(selinux_setfscreatecon,		NULL)
+	PHP_FE(selinux_setfscreatecon_raw,	NULL)
+	PHP_FE(selinux_getkeycreatecon,		NULL)
+	PHP_FE(selinux_getkeycreatecon_raw,	NULL)
+	PHP_FE(selinux_setkeycreatecon,		NULL)
+	PHP_FE(selinux_setkeycreatecon_raw,	NULL)
+	PHP_FE(selinux_getsockcreatecon,	NULL)
+	PHP_FE(selinux_getsockcreatecon_raw,	NULL)
+	PHP_FE(selinux_setsockcreatecon,	NULL)
+	PHP_FE(selinux_setsockcreatecon_raw,	NULL)
+
+	/* get/set file context */
+	PHP_FE(selinux_getfilecon,		NULL)
+	PHP_FE(selinux_getfilecon_raw,		NULL)
+	PHP_FE(selinux_lgetfilecon,		NULL)
+	PHP_FE(selinux_lgetfilecon_raw,		NULL)
+	PHP_FE(selinux_fgetfilecon,		NULL)
+	PHP_FE(selinux_fgetfilecon_raw,		NULL)
+
+	PHP_FE(selinux_setfilecon,		NULL)
+	PHP_FE(selinux_setfilecon_raw,		NULL)
+	PHP_FE(selinux_lsetfilecon,		NULL)
+	PHP_FE(selinux_lsetfilecon_raw,		NULL)
+	PHP_FE(selinux_fsetfilecon,		NULL)
+	PHP_FE(selinux_fsetfilecon_raw,		NULL)
+
+#if HAVE_SOCKETS
+	/* labeled networking  */
+	PHP_FE(selinux_getpeercon,		NULL)
+	PHP_FE(selinux_getpeercon_raw,		NULL)
+#endif
+
 	{NULL, NULL, NULL},
 };
 
@@ -63,6 +108,9 @@ PHP_FUNCTION(selinux_mls_is_enabled)
 	RETURN_FALSE;
 }
 
+/*
+ * Wrappers for the /proc/pid/attr API.
+ */
 PHP_FUNCTION(selinux_getcon)
 {
 	security_context_t context;
@@ -73,5 +121,459 @@ PHP_FUNCTION(selinux_getcon)
 	RETVAL_STRING(context, 1);
 	freecon(context);
 }
+
+PHP_FUNCTION(selinux_getcon_raw)
+{
+	security_context_t context;
+
+	if (getcon_raw(&context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_setcon)
+{
+	char *context;
+	int context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+	if (setcon(context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_setcon_raw)
+{
+	char *context;
+	int context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+
+	if (setcon_raw(context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_getpidcon)
+{
+	security_context_t context;
+	long pid;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "l", &pid) == FAILURE
+	    || getpidcon((pid_t) pid, &context) < 0)
+
+
+	if (getpidcon((pid_t) pid, &context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+        freecon(context);
+}
+
+PHP_FUNCTION(selinux_getpidcon_raw)
+{
+	security_context_t context;
+	long pid;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "l", &pid) == FAILURE)
+		RETURN_FALSE;
+
+	if (getpidcon_raw((pid_t) pid, &context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+        freecon(context);
+}
+
+PHP_FUNCTION(selinux_getprevcon)
+{
+	security_context_t context;
+
+	if (getprevcon(&context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_getprevcon_raw)
+{
+	security_context_t context;
+
+	if (getprevcon_raw(&context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_getexeccon)
+{
+	security_context_t context;
+
+	if (getexeccon(&context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_getexeccon_raw)
+{
+	security_context_t context;
+
+	if (getexeccon_raw(&context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_setexeccon)
+{
+	char *context;
+	int context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+	if (setexeccon(context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_setexeccon_raw)
+{
+	char *context;
+	int context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+	if (setexeccon_raw(context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_getfscreatecon)
+{
+	security_context_t context;
+
+	if (getfscreatecon(&context) < 0)
+		RETURN_FALSE;
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_getfscreatecon_raw)
+{
+	security_context_t context;
+
+	if (getfscreatecon_raw(&context) < 0)
+		RETURN_FALSE;
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_setfscreatecon)
+{
+	char *context;
+	int context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+	if (setfscreatecon(context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_setfscreatecon_raw)
+{
+	char *context;
+	int context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+	if (setfscreatecon_raw(context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_getkeycreatecon)
+{
+	security_context_t context;
+
+	if (getkeycreatecon(&context) < 0)
+		RETURN_FALSE;
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_getkeycreatecon_raw)
+{
+	security_context_t context;
+
+	if (getkeycreatecon_raw(&context) < 0)
+		RETURN_FALSE;
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_setkeycreatecon)
+{
+	char *context;
+	int context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+	if (setkeycreatecon(context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_setkeycreatecon_raw)
+{
+	char *context;
+	int context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+	if (setkeycreatecon_raw(context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_getsockcreatecon)
+{
+	security_context_t context;
+
+	if (getsockcreatecon(&context) < 0)
+		RETURN_FALSE;
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_getsockcreatecon_raw)
+{
+	security_context_t context;
+
+	if (getsockcreatecon_raw(&context) < 0)
+		RETURN_FALSE;
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_setsockcreatecon)
+{
+	char *context;
+	int context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+	if (setsockcreatecon(context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_setsockcreatecon_raw)
+{
+	char *context;
+	int context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+	if (setsockcreatecon_raw(context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+/*
+ * Get file context
+ */
+PHP_FUNCTION(selinux_getfilecon)
+{
+	char *filename;
+	int filename_len;
+	security_context_t context;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &filename, &filename_len) == FAILURE)
+		RETURN_FALSE;
+
+	if (getfilecon(filename, &context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_getfilecon_raw)
+{
+	char *filename;
+	int filename_len;
+	security_context_t context;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &filename, &filename_len) == FAILURE)
+		RETURN_FALSE;
+
+	if (getfilecon(filename, &context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_lgetfilecon)
+{
+	char *filename;
+	int filename_len;
+	security_context_t context;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &filename, &filename_len) == FAILURE)
+		RETURN_FALSE;
+
+	if (lgetfilecon(filename, &context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_lgetfilecon_raw)
+{
+	char *filename;
+	int filename_len;
+	security_context_t context;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+				  "s", &filename, &filename_len) == FAILURE)
+		RETURN_FALSE;
+
+	if (lgetfilecon_raw(filename, &context) < 0)
+		RETURN_FALSE;
+
+	RETVAL_STRING(context, 1);
+	freecon(context);
+}
+
+PHP_FUNCTION(selinux_fgetfilecon)
+{
+	
+}
+
+PHP_FUNCTION(selinux_fgetfilecon_raw)
+{
+	
+}
+
+/*
+ * Set file context
+ */
+PHP_FUNCTION(selinux_setfilecon)
+{
+	char *filename, *context;
+	int filename_len, context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss",
+				  &filename, &filename_len,
+				  &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+
+	if (setfilecon(filename, context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_setfilecon_raw)
+{
+	char *filename, *context;
+	int filename_len, context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss",
+				  &filename, &filename_len,
+				  &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+
+	if (setfilecon_raw(filename, context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_lsetfilecon)
+{
+	char *filename, *context;
+	int filename_len, context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss",
+				  &filename, &filename_len,
+				  &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+
+	if (lsetfilecon(filename, context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_lsetfilecon_raw)
+{
+	char *filename, *context;
+	int filename_len, context_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss",
+				  &filename, &filename_len,
+				  &context, &context_len) == FAILURE)
+		RETURN_FALSE;
+
+	if (lsetfilecon_raw(filename, context) < 0)
+		RETURN_FALSE;
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(selinux_fsetfilecon)
+{}
+
+PHP_FUNCTION(selinux_fsetfilecon_raw)
+{}
+
+#if HAVE_SOCKETS
+/*
+ * Labeled Networking
+ */
+PHP_FUNCTION(selinux_getpeercon)
+{}
+
+PHP_FUNCTION(selinux_getpeercon_raw)
+{}
+#endif
 
 #endif	/* HAVE_SELINUX */
