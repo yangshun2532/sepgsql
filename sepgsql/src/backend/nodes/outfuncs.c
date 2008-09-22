a6b24422131c404705df80a065d527b13fa3f356
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-09-22 07:43:20 +0000

    add remaining pgace private member for outfuncs.c and equalfuncs.c

--- sepgsql/src/backend/nodes/outfuncs.c
+++ sepgsql/src/backend/nodes/outfuncs.c
@@ -1432,6 +1432,7 @@ _outRelOptInfo(StringInfo str, RelOptInfo *node)
 	WRITE_BOOL_FIELD(has_eclass_joins);
 	WRITE_BITMAPSET_FIELD(index_outer_relids);
 	WRITE_NODE_FIELD(index_inner_paths);
+	WRITE_UINT_FIELD(pgaceTuplePerms);
 }
 
 static void
@@ -1601,6 +1602,7 @@ _outCreateStmt(StringInfo str, CreateStmt *node)
 	WRITE_NODE_FIELD(options);
 	WRITE_ENUM_FIELD(oncommit, OnCommitAction);
 	WRITE_STRING_FIELD(tablespacename);
+	WRITE_NODE_FIELD(pgaceItem);
 }
 
 static void
@@ -1718,6 +1720,7 @@ _outColumnDef(StringInfo str, ColumnDef *node)
 	WRITE_NODE_FIELD(raw_default);
 	WRITE_STRING_FIELD(cooked_default);
 	WRITE_NODE_FIELD(constraints);
+	WRITE_NODE_FIELD(pgaceItem);
 }
 
 static void
