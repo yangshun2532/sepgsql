71a5534e0be73ab938a9431993cbb81a0ef30b1e
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-09-27 12:51:52 +0000

    new tags (8.2.10 and 8.3.4)

--- scripts/cvspull-pgsql-8.3.x.sh
+++ scripts/cvspull-pgsql-8.3.x.sh
@@ -2,7 +2,7 @@
 
 # This script pulls the PostgreSQL tree from CVS.
 # ---- parametors ----
-CVSTAG="REL8_3_3"
+CVSTAG="REL8_3_4"
 SVNBRANCH="/branches/pgsql-8.3.x"
 
 SEPGSQL_REPOSITORY=`(cd \`dirname $0\`/..; pwd)`
