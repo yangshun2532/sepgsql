e19c2d0a9210a3b212093ed22199bc92f1fb3d2c
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-12-05 07:57:07 +0000

    merge updates of /trunk/base into /trunk/sepgsql at Fri Dec  5 16:33:12 JST 2008

--- trunk/sepgsql/src/include/catalog/catversion.h
+++ trunk/sepgsql/src/include/catalog/catversion.h
@@ -37,7 +37,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.509 2008/11/21 18:49:24 mha Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.511 2008/12/04 17:51:27 petere Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200811211
+#define CATALOG_VERSION_NO	200812041
 
 #endif
--- trunk/sepgsql/src/include/catalog/pg_proc_fn.h
+++ trunk/sepgsql/src/include/catalog/pg_proc_fn.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/pg_proc_fn.h,v 1.1 2008/03/27 03:57:34 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/pg_proc_fn.h,v 1.2 2008/12/04 17:51:27 petere Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -34,6 +34,7 @@ extern Oid ProcedureCreate(const char *procedureName,
 				Datum proconfig,
 				float4 procost,
 				float4 prorows,
+				List *parameterDefaults,
 				void *pgaceItem);
 
 extern bool function_parse_error_transpose(const char *prosrc);
