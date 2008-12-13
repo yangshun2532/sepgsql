b3e54f66c67dc96081001e4f465e004577637903
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-12-13 07:42:52 +0000

    drop trunk/rowacl-new branch

--- rowacl-new/src/include/port/univel.h
+++
@@ -1,8 +0,0 @@
-/*
- * $PostgreSQL: pgsql/src/include/port/univel.h,v 1.24 2008/05/17 01:28:24 adunstan Exp $ 
- *
- ***************************************
- * Define this if you are compiling with
- * the native UNIXWARE C compiler.
- ***************************************/
-#define USE_UNIVEL_CC
