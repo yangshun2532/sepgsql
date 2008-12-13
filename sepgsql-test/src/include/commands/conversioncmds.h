329bf916475f0cd0f074b07beb8511ad8d846d26
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-12-13 07:44:06 +0000

    a test branch for 2 system columns, a default DAC and a selectable MAC

---
+++ sepgsql-test/src/include/commands/conversioncmds.h
@@ -0,0 +1,26 @@
+/*-------------------------------------------------------------------------
+ *
+ * conversioncmds.h
+ *	  prototypes for conversioncmds.c.
+ *
+ *
+ * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ * $PostgreSQL: pgsql/src/include/commands/conversioncmds.h,v 1.17 2008/06/14 18:04:34 tgl Exp $
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef CONVERSIONCMDS_H
+#define CONVERSIONCMDS_H
+
+#include "nodes/parsenodes.h"
+
+extern void CreateConversionCommand(CreateConversionStmt *parsetree);
+extern void DropConversionsCommand(DropStmt *drop);
+extern void RenameConversion(List *name, const char *newname);
+extern void AlterConversionOwner(List *name, Oid newOwnerId);
+extern void AlterConversionOwner_oid(Oid conversionOid, Oid newOwnerId);
+
+#endif   /* CONVERSIONCMDS_H */
