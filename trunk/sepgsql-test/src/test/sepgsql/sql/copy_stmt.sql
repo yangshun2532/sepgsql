da4d55b33b5eb8c60bff025fb64f7970c0ad709f
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2008-12-16 06:50:54 +0000

    add COPY TO/FROM statement test

---
+++ trunk/sepgsql-test/src/test/sepgsql/sql/copy_stmt.sql
@@ -0,0 +1,23 @@
+-- COPY TO/FROM statement
+-- ======================
+
+COPY t1 TO stdout;
+COPY t2 TO stdout;	-- to be failed
+
+COPY t1 (security_label, a, b) TO stdout;
+COPY t2 (security_label, x, y) TO stdout;
+COPY t2 (security_label, y, z) TO stdout;	-- to be failed
+
+COPY t1 FROM stdin;
+98	yyy
+99	zzz
+\.
+
+COPY t1 (security_label, a, b) FROM stdin;
+system_u:object_r:sepgsql_table_t:s0	97	xxx
+\.
+COPY t2 (security_label, x, y) FROM stdin;	-- to be failed
+
+COPY t1 (security_label, a, b) FROM '/tmp/sepgsql_test_copy_1';	-- to be failed
+COPY t1 (security_label, a, b) FROM '/tmp/sepgsql_test_copy_2';	-- partial success
+COPY t1 (security_label, a, b) TO stdout;
