e240ee6392333fd6f35f63e7a34e649fa3e24217
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-02-20 13:22:57 +0000

    merge updates of /trunk/base into /trunk/sepgsql at r1583

--- sepgsql/src/pl/plperl/nls.mk
+++ sepgsql/src/pl/plperl/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/pl/plperl/nls.mk,v 1.1 2008/10/09 17:24:05 alvherre Exp $
+# $PostgreSQL: pgsql/src/pl/plperl/nls.mk,v 1.3 2009/02/19 10:07:58 petere Exp $
 CATALOG_NAME	:= plperl
 AVAIL_LANGUAGES	:=
-GETTEXT_FILES	:= plperl.c SPI.xs
-GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext write_stderr croak Perl_croak
+GETTEXT_FILES	:= plperl.c SPI.c
+GETTEXT_TRIGGERS:= errmsg errdetail errdetail_log errhint errcontext
