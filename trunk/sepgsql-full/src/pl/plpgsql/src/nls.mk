e638306332e7863db175c846eccfd83b8824b294
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-02-20 13:56:19 +0000

    merge updates of /trunk/base into /trunk/sepgsql-full at r1583

--- trunk/sepgsql-full/src/pl/plpgsql/src/nls.mk
+++ trunk/sepgsql-full/src/pl/plpgsql/src/nls.mk
@@ -1,8 +1,8 @@
-# $PostgreSQL: pgsql/src/pl/plpgsql/src/nls.mk,v 1.2 2008/10/09 18:15:28 alvherre Exp $
+# $PostgreSQL: pgsql/src/pl/plpgsql/src/nls.mk,v 1.5 2009/02/17 12:59:35 petere Exp $
 CATALOG_NAME	:= plpgsql
 AVAIL_LANGUAGES	:= es
 GETTEXT_FILES	:= pl_comp.c pl_exec.c pl_gram.c pl_funcs.c pl_handler.c pl_scan.c
-GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext write_stderr yyerror
+GETTEXT_TRIGGERS:= _ errmsg errdetail errdetail_log errhint errcontext validate_tupdesc_compat:3 yyerror plpgsql_yyerror
 
 .PHONY: gettext-files
 gettext-files: distprep
