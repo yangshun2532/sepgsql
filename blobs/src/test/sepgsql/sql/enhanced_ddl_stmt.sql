0c73c4f406d45b748c6baa3e0ba88995ffd80d53
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-02-23 04:43:07 +0000

    merge updates at r1592:sysatt -> {rowlv, blobs}

--- blobs/src/test/sepgsql/sql/enhanced_ddl_stmt.sql
+++ blobs/src/test/sepgsql/sql/enhanced_ddl_stmt.sql
@@ -18,15 +18,15 @@ CREATE TABLE t1 (
        a int,
        b text
 );
-SELECT relname, relselabel FROM pg_class WHERE oid = 't1'::regclass;
-SELECT attname, attselabel FROM pg_attribute WHERE attrelid = 't1'::regclass and attnum > 0;
+SELECT relname, security_label FROM pg_class WHERE oid = 't1'::regclass;
+SELECT attname, security_label FROM pg_attribute WHERE attrelid = 't1'::regclass and attnum > 0;
 
 CREATE TABLE t2 (
        a int,
        b text
 ) SECURITY_LABEL = 'unconfined_u:object_r:sepgsql_ro_table_t:s0';
-SELECT relname, relselabel FROM pg_class WHERE oid = 't2'::regclass;
-SELECT attname, attselabel FROM pg_attribute WHERE attrelid = 't2'::regclass and attnum > 0;
+SELECT relname, security_label FROM pg_class WHERE oid = 't2'::regclass;
+SELECT attname, security_label FROM pg_attribute WHERE attrelid = 't2'::regclass and attnum > 0;
 
 CREATE TABLE t3 (
        a int,
@@ -34,8 +34,8 @@ CREATE TABLE t3 (
        c bool SECURITY_LABEL = 'unconfined_u:object_r:sepgsql_secret_table_t:s0',
        d int
 ) SECURITY_LABEL = 'unconfined_u:object_r:sepgsql_table_t:s0:c0';
-SELECT relname, relselabel FROM pg_class WHERE oid = 't3'::regclass;
-SELECT attname, attselabel FROM pg_attribute WHERE attrelid = 't3'::regclass and attnum > 0;
+SELECT relname, security_label FROM pg_class WHERE oid = 't3'::regclass;
+SELECT attname, security_label FROM pg_attribute WHERE attrelid = 't3'::regclass and attnum > 0;
 
 CREATE TABLE t4 (
        a int,
@@ -50,19 +50,19 @@ CREATE TABLE t4 (
 -- ALTER TABLE with SECURITY_LABEL clause
 ALTER TABLE t2 SECURITY_LABEL = 'unconfined_u:object_r:sepgsql_secret_table_t:s0';
 ALTER TABLE t2 ADD COLUMN c bool;	-- it inherits table's one
-SELECT relname, relselabel FROM pg_class WHERE oid = 't2'::regclass;
-SELECT attname, attselabel FROM pg_attribute WHERE attrelid = 't2'::regclass and attnum > 0;
+SELECT relname, security_label FROM pg_class WHERE oid = 't2'::regclass;
+SELECT attname, security_label FROM pg_attribute WHERE attrelid = 't2'::regclass and attnum > 0;
 
 ALTER TABLE t3 ALTER b SECURITY_LABEL = 'unconfined_u:object_r:sepgsql_table_t:s0';
-SELECT relname, relselabel FROM pg_class WHERE oid = 't3'::regclass;
-SELECT attname, attselabel FROM pg_attribute WHERE attrelid = 't3'::regclass and attnum > 0;
+SELECT relname, security_label FROM pg_class WHERE oid = 't3'::regclass;
+SELECT attname, security_label FROM pg_attribute WHERE attrelid = 't3'::regclass and attnum > 0;
 
 -- CREATE FUNCTION with SECURITY_LABEL clause
 CREATE FUNCTION f1 (int) RETURNS int
        LANGUAGE 'sql'
        SECURITY_LABEL = 'unconfined_u:object_r:sepgsql_proc_t:s0:c0'
        AS 'SELECT $1 * 2';
-SELECT proname, proselabel FROM pg_proc WHERE oid = 'f1'::regproc;
+SELECT proname, security_label FROM pg_proc WHERE oid = 'f1'::regproc;
 
 CREATE FUNCTION f2 (int) RETURNS int
        LANGUAGE 'sql'
@@ -77,13 +77,13 @@ CREATE FUNCTION f2 (int) RETURNS int
 CREATE FUNCTION f2 (int) RETURNS int
        LANGUAGE 'sql'
        AS 'SELECT $1 + $1';
-SELECT proname, proselabel FROM pg_proc WHERE oid = 'f2'::regproc;
+SELECT proname, security_label FROM pg_proc WHERE oid = 'f2'::regproc;
 
 -- ALTER FUNCTION with SECURITY_LABEL clause
 ALTER FUNCTION f1(int)
       SECURITY_LABEL = 'unconfined_u:object_r:sepgsql_proc_t:s0:c1';
-SELECT proname, proselabel FROM pg_proc WHERE oid = 'f1'::regproc;
+SELECT proname, security_label FROM pg_proc WHERE oid = 'f1'::regproc;
 
 ALTER FUNCTION f2(int)
       SECURITY_LABEL = 'unconfined_u:object_r:sepgsql_proc_t:s0:c16';	-- to be denied
-SELECT proname, proselabel FROM pg_proc WHERE oid = 'f2'::regproc;
+SELECT proname, security_label FROM pg_proc WHERE oid = 'f2'::regproc;
