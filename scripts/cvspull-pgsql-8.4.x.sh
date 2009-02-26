4dcbeba4cc670511ebf6af5e7e911c80e96e7914
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-02-26 02:08:15 +0000

    fix cvspull-pgsql-8.4.x.sh: lack of "'" to close string. :(

--- scripts/cvspull-pgsql-8.4.x.sh
+++ scripts/cvspull-pgsql-8.4.x.sh
@@ -69,16 +69,16 @@ echo "svn update"
 echo
 echo "svn merge -c `expr ${SVNREV} + 2` ./sepgsql ./sysatt"
 echo "svn diff ./sysatt"
-echo "svn commit -m 'merge updates of ${SVNBRANCH}/sepgsql into ${SVNBRANCH}/sysatt at r`expr ${SVNREV} + 2` ./sysatt"
+echo "svn commit -m 'merge updates of ${SVNBRANCH}/sepgsql into ${SVNBRANCH}/sysatt at r`expr ${SVNREV} + 2`' ./sysatt"
 echo "svn update"
 echo
 echo "svn merge -c `expr ${SVNREV} + 3` ./sysatt ./rowlv"
 echo "svn diff ./rowlv"
-echo "svn commit -m 'merge updates of ${SVNBRANCH}/sysatt into ${SVNBRANCH}/rowlv at r`expr ${SVNREV} + 3` ./rowlv"
+echo "svn commit -m 'merge updates of ${SVNBRANCH}/sysatt into ${SVNBRANCH}/rowlv at r`expr ${SVNREV} + 3`' ./rowlv"
 echo "svn update"
 echo
 echo "svn merge -c `expr ${SVNREV} + 4` ./rowlv ./blobs"
 echo "svn diff ./blobs"
-echo "svn commit -m 'merge updates of ${SVNBRANCH}/rowlv into ${SVNBRANCH}/blobs at r`expr ${SVNREV} + 4` ./blobs"
+echo "svn commit -m 'merge updates of ${SVNBRANCH}/rowlv into ${SVNBRANCH}/blobs at r`expr ${SVNREV} + 4`' ./blobs"
 echo "svn update"
 echo 
