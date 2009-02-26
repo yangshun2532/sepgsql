58dedc9000d83875ada630a64aca447464f77530
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-02-26 01:52:16 +0000

    merge updates of /trunk/sysatt into /trunk/rowlv at r1620

--- rowlv/src/backend/catalog/heap.c
+++ rowlv/src/backend/catalog/heap.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/catalog/heap.c,v 1.350 2009/01/22 20:16:01 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/catalog/heap.c,v 1.351 2009/02/24 01:38:09 tgl Exp $
  *
  *
  * INTERFACE ROUTINES
@@ -82,6 +82,7 @@ static Oid AddNewRelationType(const char *typeName,
 				   Oid typeNamespace,
 				   Oid new_rel_oid,
 				   char new_rel_kind,
+				   Oid ownerid,
 				   Oid new_array_type);
 static void RelationRemoveInheritance(Oid relid);
 static void StoreRelCheck(Relation rel, char *ccname, Node *expr,
@@ -866,6 +867,7 @@ AddNewRelationType(const char *typeName,
 				   Oid typeNamespace,
 				   Oid new_rel_oid,
 				   char new_rel_kind,
+				   Oid ownerid,
 				   Oid new_array_type)
 {
 	return
@@ -874,6 +876,7 @@ AddNewRelationType(const char *typeName,
 				   typeNamespace,		/* type namespace */
 				   new_rel_oid, /* relation oid */
 				   new_rel_kind,	/* relation kind */
+				   ownerid,		/* owner's ID */
 				   -1,			/* internal size (varlena) */
 				   TYPTYPE_COMPOSITE,	/* type-type (composite) */
 				   TYPCATEGORY_COMPOSITE, /* type-category (ditto) */
@@ -1038,6 +1041,7 @@ heap_create_with_catalog(const char *relname,
 									  relnamespace,
 									  relid,
 									  relkind,
+									  ownerid,
 									  new_array_oid);
 
 	/*
@@ -1054,6 +1058,7 @@ heap_create_with_catalog(const char *relname,
 				   relnamespace,	/* Same namespace as parent */
 				   InvalidOid,	/* Not composite, no relationOid */
 				   0,			/* relkind, also N/A here */
+				   ownerid,		/* owner's ID */
 				   -1,			/* Internal size (varlena) */
 				   TYPTYPE_BASE,	/* Not composite - typelem is */
 				   TYPCATEGORY_ARRAY, /* type-category (array) */
