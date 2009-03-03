ac27e7bb602d4cefeba085855de6239be17c17e0
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-03-03 05:14:30 +0000

    improve testcases for large objects.

--- blobs/src/test/sepgsql/sql/large_object.sql
+++ blobs/src/test/sepgsql/sql/large_object.sql
@@ -1,65 +1,83 @@
 --@SECURITY_CONTEXT=unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c15
 
-SELECT lo_import('/tmp/sepgsql_test_blob1', 6001);
-SELECT lo_import('/tmp/sepgsql_test_blob1', 6002);
-SELECT lo_import('/tmp/sepgsql_test_blob1', 6003);
+-- cleanup previous tests
+SET client_min_messages TO 'error';
 
-SELECT lo_set_security(6001, 'system_u:object_r:sepgsql_blob_t:s0');
-SELECT lo_set_security(6002, 'system_u:object_r:sepgsql_ro_blob_t:s0');
-SELECT lo_set_security(6003, 'system_u:object_r:sepgsql_secret_blob_t:s0');
+DROP TABLE IF EXISTS lolabel CASCADE;
+DROP FUNCTION IF EXISTS lo_label(oid) CASCADE;
+
+RESET client_min_messages;
+
+CREATE TABLE lolabel(
+       loid	oid,
+       label	text
+);
+
+CREATE OR REPLACE FUNCTION lo_label(oid)
+       RETURNS TEXT LANGUAGE 'sql'
+       AS 'SELECT label FROM lolabel WHERE loid = $1';
+
+INSERT INTO lolabel (SELECT lo_import('/tmp/sepgsql_test_blob1'), 'normal');
+INSERT INTO lolabel (SELECT lo_import('/tmp/sepgsql_test_blob1'), 'readonly');
+INSERT INTO lolabel (SELECT lo_import('/tmp/sepgsql_test_blob1'), 'secret');
+
+SELECT lo_set_security(loid, 'system_u:object_r:sepgsql_blob_t:s0')
+       FROM lolabel WHERE label = 'normal';
+SELECT lo_set_security(loid, 'system_u:object_r:sepgsql_ro_blob_t:s0')
+       FROM lolabel WHERE label = 'readonly';
+SELECT lo_set_security(loid, 'system_u:object_r:sepgsql_secret_blob_t:s0')
+       FROM lolabel WHERE label = 'secret';
 
 --@SECURITY_CONTEXT=unconfined_u:unconfined_r:sepgsql_test_t:s0:c0
 
-SELECT lo_get_security(6001);
-SELECT lo_get_security(6002);
-SELECT lo_get_security(6003);
+SELECT lo_get_security(loid) FROM lolabel;
 
-SELECT security_label, loid, COUNT(*) FROM pg_largeobject
-       GROUP BY security_label, loid;
+SELECT security_label, lo_label(loid) AS label, COUNT(*)
+       FROM pg_largeobject GROUP BY security_label, loid ORDER by label;
 
 -- read large object
 BEGIN;
-SELECT lo_open(6001, x'40000'::int);
+SELECT lo_open(loid, x'40000'::int) FROM lolabel WHERE label = 'normal';
 SELECT loread(0, 32);
 ROLLBACK;
 
 BEGIN;
-SELECT lo_open(6002, x'40000'::int);
+SELECT lo_open(loid, x'40000'::int) FROM lolabel WHERE label = 'readonly';
 SELECT loread(0, 32);
 ROLLBACK;
 
 BEGIN;
-SELECT lo_open(6003, x'40000'::int);	-- to be denied
-SELECT loread(0, 32);
+SELECT lo_open(loid, x'40000'::int) FROM lolabel WHERE label = 'secret';
+SELECT loread(0, 32);           -- to be denied
 ROLLBACK;
 
 -- write large object
 BEGIN;
-SELECT lo_open(6001, x'20000'::int);
+SELECT lo_open(loid, x'20000'::int) FROM lolabel WHERE label = 'normal';
 SELECT lowrite(0, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 ROLLBACK;
 
 BEGIN;
-SELECT lo_open(6002, x'20000'::int);
-SELECT lowrite(0, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');	-- to be denied
+SELECT lo_open(loid, x'20000'::int) FROM lolabel WHERE label = 'readonly';
+SELECT lowrite(0, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');        -- to be denied
 ROLLBACK;
 
 BEGIN;
-SELECT lo_open(6003, x'20000'::int);
-SELECT lowrite(0, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');	-- to be denied
+SELECT lo_open(loid, x'20000'::int) FROM lolabel WHERE label = 'secret';
+SELECT lowrite(0, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');        -- to be denied
 ROLLBACK;
 
 -- create large object
 BEGIN;
-SELECT lo_create(6004);
-SELECT lo_open(6004, x'20000'::int);
+INSERT INTO lolabel (SELECT lo_create(6004), 'local');
+SELECT lo_open(loid, x'20000'::int) FROM lolabel WHERE label = 'local';
 SELECT lowrite(0, 'abcdefghijklmnopqrstuvwxyz');
 SELECT lo_close(0);
 COMMIT;
 
 -- getattr/setattr
 BEGIN;
-SELECT lo_open(6004, x'20000'::int);
+SELECT lo_open(loid, x'20000'::int) FROM lolabel WHERE label = 'local';
 SELECT lo_lseek(0, 0, 2);	-- seek to end
 SELECT lowrite(0, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 SELECT lo_tell(0);
@@ -68,23 +86,18 @@ SELECT loread(0, 50);
 SELECT lo_close(0);
 COMMIT;
 
--- import/export large object
-SELECT lo_import('/tmp/sepgsql_test_blob2', 6005);	-- to be failed
-SELECT lo_import('/tmp/sepgsql_test_blob1', 6005);
-
-SELECT lo_export(6001, '/tmp/sepgsql_test_blob2');	-- to be failed
-SELECT lo_export(6001, '/tmp/sepgsql_test_blob1');	-- to be failed
-SELECT lo_export(6005, '/tmp/sepgsql_test_blob1');
-SELECT lo_export(6005, '/dev/null');
-
 --@SECURITY_CONTEXT=unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c15
 
 -- change security label
 BEGIN;
-SELECT lo_open(6001, x'40000'::int);	-- a seed of trouble
-SELECT lo_set_security(6001, 'system_u:object_r:sepgsql_blob_t:s0:c4');
-SELECT lo_get_security(6001);
-SELECT security_label, loid, count(*) FROM pg_largeobject
-       WHERE loid = 6001
-       GROUP BY security_label, loid;
+SELECT lo_open(loid, x'40000'::int) FROM lolabel;    -- a seed of trouble
+SELECT lo_set_security(loid, 'system_u:object_r:sepgsql_blob_t:s0:c4')
+	FROM lolabel WHERE label in ('normal', 'readonly');
+SELECT lo_get_security(loid) FROM lolabel;
+SELECT security_label, lo_label(loid) AS label, count(*)
+	FROM pg_largeobject WHERE loid in (SELECT loid FROM lolabel)
+	GROUP BY security_label, loid ORDER BY label;
 ROLLBACK;
+
+-- cleanup
+SELECT lo_unlink(loid) FROM lolabel;
