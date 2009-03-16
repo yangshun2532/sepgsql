222276b7b3ee411ff9d2ab4195552a7d8096595b
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-03-16 05:14:19 +0000

    move tags/v8.4-sepgsql -> trunk/sepgsql

--- sepgsql/src/pl/plperl/nls.mk
+++
@@ -1,5 +0,0 @@
-# $PostgreSQL: pgsql/src/pl/plperl/nls.mk,v 1.3 2009/02/19 10:07:58 petere Exp $
-CATALOG_NAME	:= plperl
-AVAIL_LANGUAGES	:=
-GETTEXT_FILES	:= plperl.c SPI.c
-GETTEXT_TRIGGERS:= errmsg errdetail errdetail_log errhint errcontext
