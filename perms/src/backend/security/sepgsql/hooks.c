ff8b752c1216b31c6ae54c87066b490b770eb210
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-03-18 02:49:50 +0000

    advanced permissions tree

---
+++ perms/src/backend/security/sepgsql/hooks.c
@@ -0,0 +1,283 @@
+/*
+ * src/backend/security/sepgsql/hooks.c
+ *    SE-PostgreSQL security hooks
+ *
+ * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ */
+#include "postgres.h"
+
+#include "catalog/pg_database.h"
+#include "catalog/pg_proc.h"
+#include "miscadmin.h"
+#include "nodes/makefuncs.h"
+#include "nodes/nodes.h"
+#include "security/sepgsql.h"
+#include "storage/bufmgr.h"
+#include "utils/syscache.h"
+#include "utils/tqual.h"
+
+/*
+ * sepgsqlCheckDatabaseAccess
+ *   checks db_database:{access} permission when the client logs-in
+ *   the given database.
+ *
+ * sepgsqlCheckDatabaseSuperuser
+ *   checks db_database:{superuser} permission when the client tries
+ *   to perform as a superuser on the given databse.
+ */
+static bool
+checkDatabaseCommon(Oid database_oid, access_vector_t perms)
+{
+	const char	   *audit_name;
+	HeapTuple		tuple;
+	bool			rc;
+
+	if (!sepgsqlIsEnabled())
+		return true;
+
+	tuple = SearchSysCache(DATABASEOID,
+						   ObjectIdGetDatum(database_oid),
+						   0, 0, 0);
+	if (!HeapTupleIsValid(tuple))
+		elog(ERROR, "SELinux: cache lookup failed for database: %u",
+			 database_oid);
+
+	audit_name = sepgsqlAuditName(DatabaseRelationId, tuple);
+	rc = sepgsqlClientHasPerms(HeapTupleGetSecLabel(tuple),
+							   SEPG_CLASS_DB_DATABASE,
+							   perms,
+							   audit_name, false);
+	ReleaseSysCache(tuple);
+
+	return rc;
+}
+
+bool
+sepgsqlCheckDatabaseAccess(Oid database_oid)
+{
+	return checkDatabaseCommon(database_oid,
+							   SEPG_DB_DATABASE__ACCESS);
+}
+
+bool
+sepgsqlCheckDatabaseSuperuser(void)
+{
+	return checkDatabaseCommon(MyDatabaseId,
+							   SEPG_DB_DATABASE__SUPERUSER);
+}
+
+/*
+ * sepgsqlCheckTableLock
+ *   checks db_table:{lock} permission when the client tries to
+ *   aquire explicit lock on the given relation.
+ *
+ * sepgsqlCheckTableTruncate
+ *   checks db_table:{delete} permission when the client tries to
+ *   truncate the given relation.
+ */
+static void
+checkTableCommon(Oid table_oid, access_vector_t perms)
+{
+	const char		   *audit_name;
+	security_class_t	tclass;
+	HeapTuple			tuple;
+
+	if (!sepgsqlIsEnabled())
+		return;
+
+	tuple = SearchSysCache(RELOID,
+						   ObjectIdGetDatum(table_oid),
+						   0, 0, 0);
+	if (!HeapTupleIsValid(tuple))
+		elog(ERROR, "SELinux: cache lookup failed for relation %u", table_oid);
+
+	tclass = sepgsqlTupleObjectClass(RelationRelationId, tuple);
+	if (tclass == SEPG_CLASS_DB_TABLE)
+	{
+		audit_name = sepgsqlAuditName(RelationRelationId, tuple);
+		sepgsqlClientHasPerms(HeapTupleGetSecLabel(tuple),
+							  SEPG_CLASS_DB_TABLE,
+							  perms,
+							  audit_name, true);
+	}
+	ReleaseSysCache(tuple);
+}
+
+void
+sepgsqlCheckTableLock(Oid table_oid)
+{
+	/* check db_table:{lock} permission */
+	checkTableCommon(table_oid, SEPG_DB_TABLE__LOCK);
+}
+
+void
+sepgsqlCheckTableTruncate(Relation rel)
+{
+	HeapScanDesc		scan;
+	HeapTuple			tuple;
+	security_class_t	tclass;
+	const char		   *audit_name;
+
+	/* check db_table:{delete} permission */
+	checkTableCommon(RelationGetRelid(rel), SEPG_DB_TABLE__DELETE);
+
+	/* check db_tuple:{delete} permission */
+	scan = heap_beginscan(rel, SnapshotNow, 0, NULL);
+
+	while ((tuple = heap_getnext(scan, ForwardScanDirection)) != NULL)
+	{
+		tclass = sepgsqlTupleObjectClass(RelationGetRelid(rel), tuple);
+		audit_name = sepgsqlAuditName(RelationGetRelid(rel), tuple);
+		sepgsqlClientHasPerms(HeapTupleGetSecLabel(tuple),
+							  tclass,
+							  SEPG_DB_TUPLE__DELETE,
+							  audit_name, true);
+	}
+	heap_endscan(scan);
+}
+
+/*
+ * sepgsqlCheckProcedureExecute
+ *   checks db_procedure:{execute} permission when the client tries
+ *   to invoke the given SQL function.
+ */
+bool sepgsqlCheckProcedureExecute(Oid proc_oid)
+{
+	const char *audit_name;
+	HeapTuple tuple;
+	bool rc;
+
+	if (!sepgsqlIsEnabled())
+		return true;
+
+	/*
+	 * check db_procedure:{execute} permission
+	 */
+	tuple = SearchSysCache(PROCOID,
+						   ObjectIdGetDatum(proc_oid),
+						   0, 0, 0);
+	if (!HeapTupleIsValid(tuple))
+		elog(ERROR, "SELinux: cache lookup failed for procedure: %u", proc_oid);
+
+	audit_name = sepgsqlAuditName(ProcedureRelationId, tuple);
+	rc = sepgsqlClientHasPerms(HeapTupleGetSecLabel(tuple),
+							   SEPG_CLASS_DB_PROCEDURE,
+							   SEPG_DB_PROCEDURE__EXECUTE,
+							   audit_name, false);
+	ReleaseSysCache(tuple);
+
+	return rc;
+}
+
+/*
+ * sepgsqlCheckProcedureEntrypoint
+ *   checks whether the given function call causes domain transition,
+ *   or not. If it needs a domain transition, it injects a wrapper
+ *   function to invoke it under new domain.
+ */
+struct TrustedProcedureCache
+{
+	FmgrInfo	flinfo;
+	char		newcon[1];
+};
+
+static Datum
+sepgsqlTrustedProcedure(PG_FUNCTION_ARGS)
+{
+	struct TrustedProcedureCache *tcache;
+	security_context_t	save_context;
+	FmgrInfo		   *save_flinfo;
+	Datum				result;
+
+	tcache = fcinfo->flinfo->fn_extra;
+	Assert(tcache != NULL);
+
+	save_context = sepgsqlSwitchClient(tcache->newcon);
+	save_flinfo = fcinfo->flinfo;
+	fcinfo->flinfo = &tcache->flinfo;
+
+	PG_TRY();
+	{
+		result = FunctionCallInvoke(fcinfo);
+	}
+	PG_CATCH();
+	{
+		sepgsqlSwitchClient(save_context);
+		fcinfo->flinfo = save_flinfo;
+		PG_RE_THROW();
+	}
+	PG_END_TRY();
+	sepgsqlSwitchClient(save_context);
+	fcinfo->flinfo = save_flinfo;
+
+	return result;
+}
+
+void
+sepgsqlCheckProcedureEntrypoint(FmgrInfo *flinfo, HeapTuple protup)
+{
+	struct TrustedProcedureCache *tcache;
+	security_context_t	newcon;
+	const char		   *audit_name;
+
+	if (!sepgsqlIsEnabled())
+		return;
+
+	newcon = sepgsqlClientCreateLabel(HeapTupleGetSecLabel(protup),
+									  SEPG_CLASS_PROCESS);
+
+	/* Do nothing, if it is not a trusted procedure */
+	if (strcmp(newcon, sepgsqlGetClientLabel()) == 0)
+		return;
+
+	/* check db_procedure:{entrypoint} */
+	audit_name = sepgsqlAuditName(ProcedureRelationId, protup);
+	sepgsqlClientHasPerms(HeapTupleGetSecLabel(protup),
+						  SEPG_CLASS_DB_PROCEDURE,
+						  SEPG_DB_PROCEDURE__ENTRYPOINT,
+						  audit_name, true);
+
+	/* check process:{transition} */
+	sepgsqlComputePerms(sepgsqlGetClientLabel(),
+						newcon,
+						SEPG_CLASS_PROCESS,
+						SEPG_PROCESS__TRANSITION,
+						NULL, true);
+
+	/* setup trusted procedure */
+	tcache = MemoryContextAllocZero(flinfo->fn_mcxt,
+							sizeof(*tcache) + strlen(newcon));
+	memcpy(&tcache->flinfo, flinfo, sizeof(*flinfo));
+	strcmp(tcache->newcon, newcon);
+	flinfo->fn_addr = sepgsqlTrustedProcedure;
+	flinfo->fn_extra = tcache;
+}
+
+/*
+ * sepgsqlAllowFunctionInlined
+ *   It provides the optimizer a hint whether the given SQL function
+ *   can be inlined, or not. If it can be configured as a trusted
+ *   procedure, we should not allow it inlined.
+ */
+bool
+sepgsqlAllowFunctionInlined(HeapTuple proc_tuple)
+{
+	security_context_t	context;
+
+	if (!sepgsqlIsEnabled())
+		return true;
+
+	context = sepgsqlClientCreateLabel(HeapTupleGetSecLabel(proc_tuple),
+									   SEPG_CLASS_PROCESS);
+	/*
+	 * If the security context of client is unchange
+	 * before or after invocation of the functions,
+	 * it is not a trusted procedure, so it can be
+	 * inlined due to performance purpose.
+	 */
+	if (strcmp(sepgsqlGetClientLabel(), context) == 0)
+		return true;
+
+	return false;
+}
---
+++ perms/src/backend/security/sepgsql/label.c
@@ -0,0 +1,311 @@
+/*
+ * src/backend/security/sepgsql/label.c
+ *    SE-PostgreSQL security label management
+ *
+ * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ */
+#include "postgres.h"
+
+#include "access/xact.h"
+#include "catalog/catalog.h"
+#include "catalog/indexing.h"
+#include "catalog/pg_attribute.h"
+#include "catalog/pg_class.h"
+#include "catalog/pg_database.h"
+#include "catalog/pg_proc.h"
+#include "catalog/pg_security.h"
+#include "catalog/pg_type.h"
+#include "miscadmin.h"
+#include "nodes/makefuncs.h"
+#include "security/sepgsql.h"
+#include "utils/builtins.h"
+#include "utils/memutils.h"
+#include "utils/syscache.h"
+
+/* GUC: to turn on/off row level controls in SE-PostgreSQL */
+bool sepostgresql_row_level;
+
+/*
+ * sepgsqlTupleDescHasSecLabel
+ *
+ *   returns a hint whether we should allocate a field to store
+ *   security label on the given relation, or not.
+ */
+bool
+sepgsqlTupleDescHasSecLabel(Relation rel)
+{
+	if (!sepgsqlIsEnabled())
+		return false;
+
+	if (rel == NULL)
+		return false;	/* target of SELECT INTO */
+
+	if (RelationGetRelid(rel) == DatabaseRelationId  ||
+		RelationGetRelid(rel) == RelationRelationId  ||
+		RelationGetRelid(rel) == AttributeRelationId ||
+		RelationGetRelid(rel) == ProcedureRelationId)
+		return true;
+
+	return sepostgresql_row_level;
+}
+
+/*
+ * sepgsqlSetDefaultSecLabel 
+ *
+ *   assigns a default security context for the newly inserted tuple.
+ */
+extern void
+sepgsqlSetDefaultSecLabel(Relation rel, HeapTuple tuple)
+{
+	security_class_t	tclass;
+	security_context_t	context;
+	Form_pg_attribute	attForm;
+	HeapTuple			reltup;
+	Oid					newsid, table_sid;
+
+	Assert(HeapTupleHasSecLabel(tuple));
+	tclass = sepgsqlTupleObjectClass(RelationGetRelid(rel), tuple);
+
+	switch (tclass)
+	{
+	case SEPG_CLASS_DB_DATABASE:
+		context = sepgsqlComputeCreate(sepgsqlGetClientLabel(),
+									   sepgsqlGetClientLabel(),
+									   SEPG_CLASS_DB_DATABASE);
+		newsid = securityLookupSecurityId(context);
+		break;
+
+	case SEPG_CLASS_DB_TABLE:
+		newsid = sepgsqlClientCreate(sepgsqlGetDatabaseSid(),
+									 SEPG_CLASS_DB_TABLE);
+		break;
+
+	case SEPG_CLASS_DB_PROCEDURE:
+		newsid = sepgsqlClientCreate(sepgsqlGetDatabaseSid(),
+									 SEPG_CLASS_DB_PROCEDURE);
+		break;
+
+	case SEPG_CLASS_DB_COLUMN:
+		attForm = (Form_pg_attribute) GETSTRUCT(tuple);
+		if (IsBootstrapProcessingMode() &&
+			(attForm->attrelid == TypeRelationId ||
+			 attForm->attrelid == ProcedureRelationId ||
+			 attForm->attrelid == AttributeRelationId ||
+			 attForm->attrelid == RelationRelationId))
+		{
+			table_sid = sepgsqlClientCreate(sepgsqlGetDatabaseSid(),
+											SEPG_CLASS_DB_TABLE);
+		}
+		else
+		{
+			reltup = SearchSysCache(RELOID,
+									ObjectIdGetDatum(attForm->attrelid),
+									0, 0, 0);
+			if (!HeapTupleIsValid(reltup))
+				elog(ERROR, "SELinux: cache lookup failed fro relation: %u",
+					 attForm->attrelid);
+			
+			table_sid = HeapTupleGetSecLabel(reltup);
+			ReleaseSysCache(reltup);
+		}
+		newsid = sepgsqlClientCreate(table_sid, SEPG_CLASS_DB_COLUMN);
+       break;
+
+	default:	/* SEPG_CLASS_DB_TUPLE */
+		if (IsBootstrapProcessingMode() &&
+			(RelationGetRelid(rel) == TypeRelationId ||
+			 RelationGetRelid(rel) == ProcedureRelationId ||
+			 RelationGetRelid(rel) == AttributeRelationId ||
+			 RelationGetRelid(rel) == RelationRelationId))
+		{
+			table_sid = sepgsqlClientCreate(sepgsqlGetDatabaseSid(),
+											SEPG_CLASS_DB_TABLE);
+		}
+		else
+		{
+			reltup = SearchSysCache(RELOID,
+									ObjectIdGetDatum(RelationGetRelid(rel)),
+									0, 0, 0);
+			if (!HeapTupleIsValid(reltup))
+				elog(ERROR, "SELinux: cache lookup failed fro relation: %u",
+					 RelationGetRelid(rel));
+			table_sid = HeapTupleGetSecLabel(reltup);
+			ReleaseSysCache(reltup);
+		}
+		newsid = sepgsqlClientCreate(table_sid, SEPG_CLASS_DB_TUPLE);
+		break;
+	}
+
+	HeapTupleSetSecLabel(tuple, newsid);
+}
+
+/*
+ * sepgsqlMetaSecurityLabel
+ *   It returns a security label of tuples within pg_security system
+ *   catalog. The purpose of this special handling is to avoid infinite
+ *   function invocations to insert new entry for meta security labels.
+ */
+char *
+sepgsqlMetaSecurityLabel(void)
+{
+	HeapTuple			tuple;
+	security_context_t	tcontext;
+	Oid					tsecid;
+
+	tuple = SearchSysCache(RELOID,
+						   ObjectIdGetDatum(SecurityRelationId),
+						   0, 0, 0);
+	if (!HeapTupleIsValid(tuple))
+		elog(ERROR, "SELinux: cache lookup failed for relation: pg_security");
+
+	tsecid = HeapTupleGetSecLabel(tuple);
+
+	ReleaseSysCache(tuple);
+
+	tcontext = securityLookupSecurityLabel(tsecid);
+	if (!tcontext || security_check_context(tcontext) < 0)
+		tcontext = sepgsqlGetUnlabeledLabel();
+
+	return sepgsqlComputeCreate(sepgsqlGetServerLabel(),
+								tcontext,
+								SEPG_CLASS_DB_TUPLE);
+}
+
+/*
+ * sepgsqlInputGivenSecLabel
+ *   translate a given security label in text form into a security
+ *   identifier. It can raise an error, if its format is violated,
+ *   but permission checks are done later.
+ */
+Oid
+sepgsqlInputGivenSecLabel(DefElem *defel)
+{
+	security_context_t	context;
+
+	if (!defel)
+		return InvalidOid;
+
+	if (!sepgsqlIsEnabled())
+		ereport(ERROR,
+				(errcode(ERRCODE_SELINUX_ERROR),
+				 errmsg("SELinux: disabled now")));
+
+	context = strVal(defel->arg);
+	if (security_check_context(context) < 0)
+		ereport(ERROR,
+				(errcode(ERRCODE_SELINUX_ERROR),
+				 errmsg("SELinux: not a valid security context: \"%s\"",
+						context)));
+
+	return securityTransSecLabelIn(context);
+}
+
+/*
+ * sepgsqlInputGivenSecLabelRelation
+ *   organize a set of given security labels on CREATE TABLE statement.
+ *   User can specify a security label for individual table/columns.
+ *   It returns a list of DefElem. !defel->defname means a specified one
+ *   for the table, rest of them means one for columns.
+ */
+List *
+sepgsqlInputGivenSecLabelRelation(CreateStmt *stmt)
+{
+	List	   *results = NIL;
+	ListCell   *l;
+	DefElem	   *defel, *newel;
+
+	if (stmt->secLabel)
+	{
+		defel = (DefElem *) stmt->secLabel;
+
+		Assert(IsA(defel, DefElem));
+
+		newel = makeDefElem(NULL, copyObject(defel->arg));
+		results = lappend(results, newel);
+	}
+
+	foreach (l, stmt->tableElts)
+	{
+		ColumnDef  *cdef = lfirst(l);
+
+		if (cdef->secLabel)
+		{
+			defel = (DefElem *) cdef->secLabel;
+
+			Assert(IsA(defel, DefElem));
+
+			newel = makeDefElem(pstrdup(cdef->colname),
+								copyObject(defel->arg));
+			results = lappend(results, newel);
+		}
+	}
+
+	return results;
+}
+
+/*
+ * sepgsqlSecurityLabelTransIn()
+ *   translate external security label into internal one
+ */
+security_context_t
+sepgsqlSecurityLabelTransIn(security_context_t seclabel)
+{
+	security_context_t	rawlabel;
+	security_context_t	result;
+
+	if (!sepgsqlIsEnabled())
+		return seclabel;
+
+	if (selinux_trans_to_raw_context(seclabel, &rawlabel) < 0)
+		ereport(ERROR,
+				(errcode(ERRCODE_SELINUX_ERROR),
+				 errmsg("SELinux: failed to translate \"%s\" to raw format",
+						seclabel)));
+	PG_TRY();
+	{
+		result = pstrdup(rawlabel);
+	}
+	PG_CATCH();
+	{
+		freecon(rawlabel);
+		PG_RE_THROW();
+	}
+	PG_END_TRY();
+	freecon(rawlabel);
+
+	return result;
+}
+
+/*
+ * sepgsqlSecurityLabelTransOut()
+ *   translate internal security label into external one
+ */
+security_context_t
+sepgsqlSecurityLabelTransOut(security_context_t rawlabel)
+{
+	security_context_t	seclabel;
+	security_context_t	result;
+
+	if (!sepgsqlIsEnabled())
+		return rawlabel;
+
+	if (selinux_raw_to_trans_context(rawlabel, &seclabel) < 0)
+		ereport(ERROR,
+                (errcode(ERRCODE_SELINUX_ERROR),
+                 errmsg("SELinux: failed to translate \"%s\" to readable format",
+                        rawlabel)));
+	PG_TRY();
+	{
+		result = pstrdup(seclabel);
+	}
+	PG_CATCH();
+	{
+		freecon(seclabel);
+		PG_RE_THROW();
+	}
+	PG_END_TRY();
+	freecon(seclabel);
+
+	return result;
+}
