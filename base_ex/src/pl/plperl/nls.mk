cecabffcd40d692201565bcb8e551e7fa8203fe7
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-04-22 23:30:20 +0000

    remove obsolete idea tree

--- base_ex/src/pl/plperl/nls.mk
+++
@@ -1,5 +0,0 @@
-# $PostgreSQL: pgsql/src/pl/plperl/nls.mk,v 1.4 2009/04/09 19:38:53 petere Exp $
-CATALOG_NAME	:= plperl
-AVAIL_LANGUAGES	:= de es fr
-GETTEXT_FILES	:= plperl.c SPI.c
-GETTEXT_TRIGGERS:= errmsg errdetail errdetail_log errhint errcontext
--- base_ex/src/pl/tcl/nls.mk
+++
@@ -1,5 +0,0 @@
-# $PostgreSQL: pgsql/src/pl/tcl/nls.mk,v 1.3 2009/04/09 19:38:53 petere Exp $
-CATALOG_NAME	:= pltcl
-AVAIL_LANGUAGES	:= de es fr
-GETTEXT_FILES	:= pltcl.c
-GETTEXT_TRIGGERS:= errmsg errdetail errdetail_log errhint errcontext
