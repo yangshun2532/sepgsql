20a121502865e582578636eb769a2d8256652dda
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-04-22 23:30:20 +0000

    remove obsolete idea tree

--- trunk/base_ex/src/bin/pg_controldata/nls.mk
+++
@@ -1,5 +0,0 @@
-# $PostgreSQL: pgsql/src/bin/pg_controldata/nls.mk,v 1.18 2009/04/09 19:38:51 petere Exp $
-CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= cs de es fa fr hu it ja ko nb pl pt_BR ro ru sk sl sv ta tr zh_CN zh_TW
-GETTEXT_FILES	:= pg_controldata.c
-GETTEXT_TRIGGERS:= _
