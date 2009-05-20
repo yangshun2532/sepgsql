672692ae571176701b42130a425730ccae0e3fb8
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-05-20 07:58:16 +0000

    Lack of sepgsqlIsEnabled() in some hooks

--- trunk/perms/src/backend/security/sepgsql/hooks.c
+++ trunk/perms/src/backend/security/sepgsql/hooks.c
@@ -225,6 +225,9 @@ sepgsqlCheckSchemaCommon(Oid nsid, access_vector_t required, bool abort)
 
 bool sepgsqlCheckSchemaSearch(Oid nsid)
 {
+	if (!sepgsqlIsEnabled())
+		return true;
+
 	return sepgsqlCheckSchemaCommon(nsid, SEPG_DB_SCHEMA__SEARCH, false);
 }
 
@@ -412,6 +415,9 @@ sepgsqlCheckSequenceCommon(Oid seqid, access_vector_t required)
 {
 	const char *audit_name;
 	HeapTuple tuple;
+
+	if (!sepgsqlIsEnabled())
+		return;
 
 	tuple = SearchSysCache(RELOID,
 						   ObjectIdGetDatum(seqid),
