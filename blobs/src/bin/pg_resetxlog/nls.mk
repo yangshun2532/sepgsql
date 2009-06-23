955b2804027a520ad9fd0bf3f889bc62e41f1151
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-06-23 00:27:01 +0000

    /trunk/blobs to be rebased

--- blobs/src/bin/pg_resetxlog/nls.mk
+++
@@ -1,5 +0,0 @@
-# $PostgreSQL: pgsql/src/bin/pg_resetxlog/nls.mk,v 1.19 2009/04/09 19:38:51 petere Exp $
-CATALOG_NAME	:= pg_resetxlog
-AVAIL_LANGUAGES	:= cs de es fr hu it ja ko nb pt_BR ro ru sk sl sv ta tr zh_CN zh_TW
-GETTEXT_FILES	:= pg_resetxlog.c
-GETTEXT_TRIGGERS:= _
