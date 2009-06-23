7ac286575abb516ae92f77b22a50ffcdd2d3f4fa
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-06-23 00:28:45 +0000

    security checks for largeobjects, to be based on row-level ACLs.

---
+++ trunk/blobs/src/include/catalog/pg_security.h
@@ -0,0 +1,109 @@
+/*
+ * src/include/catalog/pg_security.h
+ *    Definition of the security label relation (pg_security)
+ *
+ * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ */
+#ifndef PG_SECURITY_H
+#define PG_SECURITY_H
+
+#include "catalog/genbki.h"
+
+#include "access/htup.h"
+#include "nodes/parsenodes.h"
+#include "utils/acl.h"
+#include "utils/relcache.h"
+
+#define SecurityRelationId        3405
+
+CATALOG(pg_security,3405)
+{
+	/* OID of the table which refers the entry */
+	Oid		relid;
+
+	/* 'a' = security_acl, 'l' = security_label */
+	char	seckind;
+
+	/* Text representation of security attribute */
+	text	secattr;
+} FormData_pg_security;
+
+/*
+ * Form_pg_security corresponds to a pointer to a tuple with
+ * the format of pg_security relation.
+ */
+typedef FormData_pg_security *Form_pg_security;
+
+/*
+ * Compiler constants for pg_security
+ */
+#define Natts_pg_security				3
+#define Anum_pg_security_relid			1
+#define Anum_pg_security_seckind		2
+#define Anum_pg_security_secattr		3
+
+/*
+ * Compiler constants for pg_security.seckind
+ */
+#define SECKIND_SECURITY_ACL			'a'
+#define SECKIND_SECURITY_LABEL			'l'
+
+/*
+ * Functions to translate between security label and identifier
+ */
+extern void
+securityPostBootstrapingMode(void);
+
+extern bool
+securityTupleDescHasRowAcl(Relation rel);
+
+extern bool
+securityTupleDescHasSecLabel(Relation rel);
+
+extern Oid
+securityRawSecLabelIn(Oid relid, char *seclabel);
+
+extern char *
+securityRawSecLabelOut(Oid relid, Oid secid);
+
+extern Oid
+securityTransSecLabelIn(Oid relid, char *seclabel);
+
+extern char *
+securityTransSecLabelOut(Oid relid, Oid secid);
+
+extern Oid
+securityTransRowAclIn(Oid relid, Acl *acl);
+
+extern Acl *
+securityTransRowAclOut(Oid relid, Oid secid, Oid ownid);
+
+extern Datum
+securityHeapGetRowAclSysattr(HeapTuple tuple);
+
+extern Datum
+securityHeapGetSecLabelSysattr(HeapTuple tuple);
+
+extern void
+securityReclaimOnDropTable(Oid relid);
+
+extern Datum
+security_reclaim_acl(PG_FUNCTION_ARGS);
+
+extern Datum
+security_reclaim_table_acl(PG_FUNCTION_ARGS);
+
+extern Datum
+security_reclaim_label(PG_FUNCTION_ARGS);
+
+extern Datum
+security_reclaim_table_label(PG_FUNCTION_ARGS);
+
+extern Datum
+security_acl_to_secid(PG_FUNCTION_ARGS);
+
+extern Datum
+security_label_to_secid(PG_FUNCTION_ARGS);
+
+#endif		/* PG_SECURITY_H */
