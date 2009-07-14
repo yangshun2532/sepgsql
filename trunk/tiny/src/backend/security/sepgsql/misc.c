c3a2d5b377b2398d6d0444cd04c8ff32402df5f0
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-07-14 11:10:26 +0000

    workaround commit for SE-PgSQL/tiny

---
+++ trunk/tiny/src/backend/security/sepgsql/misc.c
@@ -0,0 +1,134 @@
+/*
+ * src/backend/security/sepgsql/misc.c
+ *    Miscellaneous facilities in SE-PostgreSQL
+ *
+ * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ */
+#include "postgres.h"
+
+
+
+static security_context_t clientLabel = NULL;
+static security_context_t serverLabel = NULL;
+
+const char *
+sepgsqlSetServerLabel(void)
+{
+	if (!serverLabel)
+	{
+		if (getcon_raw(&serverLabel) < 0)
+			ereport(ERROR,
+					(errcode(ERRCODE_SELINUX_ERROR),
+					 errmsg("SELinux: could not get server label")));
+	}
+	return serverLabel;
+}
+
+const char *
+sepgsqlGetClientLabel(void)
+{
+	if (!clientLabel)
+	{
+		/*
+		 * When the process is not invoked as a backend of client,
+		 * it works as a server process and as a client process
+		 * in same time.
+		 */
+		if (!MyProcPort)
+			return sepgsqlGetServerLabel();
+
+		/*
+		 * SELinux provides getpeercon(3) which enables to obtain
+		 * the security context of peer process.
+		 * If MyProcPort->sock is unix domain socket, no special
+		 * configuration is necessary. If it is tcp/ip socket,
+		 * labeled IPsec or fallback context to be configured.
+		 */
+		if (getpeercon_raw(MyProcPort->sock, &clientLabel) < 0)
+			ereport(ERROR,
+					(errcode(ERRCODE_SELINUX_ERROR),
+					 errmsg("SELinux: could not obtain client label")));
+	}
+	return clientLabel;
+}
+
+const char *
+sepgsqlSwitchClient(const char *new_label)
+{
+	const char *old_label = sepgsqlGetClientLabel();
+
+	clientLabel = new_label;
+
+	return old_label;
+}
+
+/*
+ * sepgsqlIsEnabled
+ *
+ * It returns true, if SE-PostgreSQL is available.
+ * All the security hooks shall check sepgsqlIsEnabled() on the head.
+ * This status is determined with two factors. The one is GUC option
+ * "sepostgresql=on/off", and the other is SELinux's status on the
+ * system. Both of them must be enabled, when SE-PostgreSQL is available.
+ */
+bool sepostgresql_enabled;
+
+bool
+sepgsqlIsEnabled(void)
+{
+	static int enabled = -1;	/* unchecked */
+
+	if (!sepostgresql_enabled)
+		return false;
+
+	if (enabled < 0)
+		enabled = is_selinux_enabled();
+
+	return enabled > 0 ? true : false;
+}
+
+/*
+ * sepgsql_getcon
+ *
+ * It returns security label of the client process.
+ */
+Datum
+sepgsql_getcon(PG_FUNCTION_ARGS)
+{
+	security_context_t context;
+
+	if (!sepgsqlIsEnabled())
+		ereport(ERROR,
+				(errcode(ERRCODE_SELINUX_ERROR),
+				 errmsg("SELinux: disabled now")));
+
+	context = sepgsqlGetClientLabel();
+	context = sepgsqlTransSecLabelOut(context);
+	return CStringGetTextDatum(context);
+}
+
+/*
+ * sepgsql_server_getcon
+ *
+ * It returns security label of the server process.
+ */
+Datum
+sepgsql_server_getcon(PG_FUNCTION_ARGS)
+{
+	security_context_t context;
+
+	if (!sepgsqlIsEnabled())
+		ereport(ERROR,
+				(errcode(ERRCODE_SELINUX_ERROR),
+				 errmsg("SELinux: disabled now")));
+
+	context = sepgsqlGetServerLabel();
+	context = sepgsqlTransSecLabelOut(context);
+	return CStringGetTextDatum(context);
+}
+
+
+
+
+
