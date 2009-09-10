691656025e1afbb4e670880725a3e0fdcf6ecc6a
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-09-10 08:38:10 +0000

    policy scripts

--- misc/policy/mkpolicy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -e "$1" ]; then
-	echo "usage: $0 <selinux-policy-xxx.src.rpm>"
-fi
-
-
-
--- scripts/build-policy.sh
+++ scripts/build-policy.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-# -- SE-PostgreSQL repository
-SEPGSQL_REPOSITORY=`(cd \`dirname $0\`/..; pwd)`
-SEPGSQL_BRANCH="/branches/pgsql-8.2.x"
+BASE_DIR=`(cd \`dirname $0\`/..; pwd)`
+SRPMFILE="$1"
 
-if [ -z "$1" ]; then
+if [ -z "$SRPMFILE" ]; then
     echo "usage: $0 <selinux-policy.xxx.src.rpm>"
     exit 1
 fi
 
-if echo $1 | egrep -q "^(http|ftp)://"; then
-    SRPMFILE=`mktemp`
-    wget -O ${SRPMFILE} $1 || exit 1
+if echo $SRPMFILE | egrep -q "^(http|ftp)://"; then
+    TEMPFILE=`mktemp`
+    wget -O "$TEMPFILE" "$SRPMFILE" || exit 1
+    SRPMFILE="$TEMPFILE"
 fi
 
 # -- unpack source rpm
@@ -22,42 +22,64 @@ cd ${SOURCE_DIR}
 rm -f `rpm -qpl ${SRPMFILE}`
 
 rpm2cpio ${SRPMFILE} | cpio -id || exit 1
+cp ${BASE_DIR}/scripts/serefpolicy-sepgsql.patch ./
 if [ ! -f selinux-policy.spec ]; then
     echo "selinux-policy.spec not found";
     exit 1
 fi
 
-# export repository
-POLICY_REPO="`mktemp -d`/package"
-svn export "${SEPGSQL_REPOSITORY}${SEPGSQL_BRANCH}/package" ${POLICY_REPO}
+# -- modify specfile
+TEMPFILE=`mktemp` || exit 1
 
-# modify specfile
-if rpm -qp --qf "%{release}" "${SRPMFILE}" | egrep -q '\.fc7$'; then
-    # Fedora 7
-    TEMPFILE=`mktemp`
-    cp ${POLICY_REPO}/refpolicy-add-sepgsql-definitions.fedora7.patch ${SOURCE_DIR}
-    cp ${POLICY_REPO}/refpolicy-add-userdomain-pgsql-connect.fedora7.patch ${SOURCE_DIR}
-    cat selinux-policy.spec                                                                     \
-        | sed 's/%{?dist}/.sepgsql%{?dist}/g'                                                   \
-        | awk "BEGIN { a = b = 0; }                                                             \
-               /^patch[0-9]*:/ { a++; print; next; }                                            \
-               a > 0 { a = 0;                                                                   \
-                       print \"patch98: refpolicy-add-sepgsql-definitions.fedora7.patch\";      \
-                       print \"patch99: refpolicy-add-userdomain-pgsql-connect.fedora7.patch\"; \
-                       print; next; }                                                           \
-               /^%patch[0-9]*/ { b++; print; next; }                                            \
-               b > 0 { b = 0;                                                                   \
-                       print \"%patch98 -p1\";                                                  \
-                       print \"%patch99 -p1\";                                                  \
-                       print; next; }                                                           \
-               { print; }" > $TEMPFILE
-    cp $TEMPFILE selinux-policy.spec && rm -f $TEMPFILE
-else
-    echo "Only Fedora 7 need to add object class definitions"
-    echo "In Fedora 8 or later, the upstreamed selinux-policy contains"
-    echo "object class definitions."
-    exit 1
-fi
+cat selinux-policy.spec \
+    | awk      "BEGIN { a = b = 0 }					\
+		/^Source[0-9]+:/ && a == 0			       	\
+		{ a = 1;						\
+		  print \"patch99: serefpolicy-sepgsql.patch\";		\
+		  print; next; }					\
+		/%patch[0-9]*/ && b == 0				\
+		{ b = 1;						\
+		  print;						\
+		  print \"%patch99 -p1\";				\
+		  next;							\
+		}							\
+		/^%clean/						\
+		{							\
+		  print \"%if %{BUILD_TARGETED}\";			\
+		  print \"install -m 644 %{buildroot}%{_usr}/share/selinux/targeted/base.pp.bz2 \\\\\";	\
+		  print \"    %{buildroot}%{_usr}/share/selinux/packages/base-sepgsql.targeted.pp.bz2\";\
+		  print \"%endif\";					\
+		  print \"%if %{BUILD_MLS}\";				\
+		  print \"install -m 644 %{buildroot}%{_usr}/share/selinux/mls/base.pp.bz2 \\\\\";	\
+		  print \"    %{buildroot}%{_usr}/share/selinux/packages/base-sepgsql.mls.pp.bz2\";	\
+		  print \"%endif\";					\
+		  print \"%if %{BUILD_MINIMUM}\";			\
+		  print \"install -m 644 %{buildroot}%{_usr}/share/selinux/minimum/base.pp.bz2 \\\\\";	\
+		  print	\"    %{buildroot}%{_usr}/share/selinux/packages/base-sepgsql.minimum.pp.bz2\";	\
+		  print \"%endif\";					\
+		  print \"\";						\
+		  print; next;						\
+		}							\
+		/^%changelog/						\
+		{							\
+		  print \"%package sepgsql\";				\
+		  print \"Summary: SELinux experimental database policy\";		\
+		  print \"Group: System Environment/Base\";		\
+		  print \"Requires(pre): selinux-policy = %{version}-%{release}\";	\
+		  print \"\";						\
+		  print \"%description sepgsql\";			\
+		  print \"SELinux experimental database policy\";	\
+		  print \"\";						\
+		  print \"%files sepgsql\";				\
+		  print \"%{_usr}/selinux/packages/base-sepgsql.*.pp.bz2\";	\
+		  print \"\";						\
+		  print; next;						\
+		}							\
+		{							\
+		  print;						\
+		}" > $TEMPFILE
+cp $TEMPFILE selinux-policy.spec
 
 # rpmbuild 
-rpmbuild -ba ${SOURCE_DIR}/selinux-policy.spec
+rpmbuild -ba ${SOURCE_DIR}/selinux-policy.spec	\
+	-D "BUILD_MINIMUM 0" -D "BUILD_OLPC 0" -D "BUILD_MLS 1"
