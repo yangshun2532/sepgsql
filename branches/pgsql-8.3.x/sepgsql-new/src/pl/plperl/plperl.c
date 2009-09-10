aba2f0b366fba9c82b0e875ecdb8f578e78fd816
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-09-10 11:24:37 +0000

    merge updates of /branches/pgsql-8.3.x/base into /branches/pgsql-8.3.x/sepgsql-new at 2298

--- branches/pgsql-8.3.x/sepgsql-new/src/pl/plperl/plperl.c
+++ branches/pgsql-8.3.x/sepgsql-new/src/pl/plperl/plperl.c
@@ -1,7 +1,7 @@
 /**********************************************************************
  * plperl.c - perl as a procedural language for PostgreSQL
  *
- *	  $PostgreSQL: pgsql/src/pl/plperl/plperl.c,v 1.136 2008/01/23 00:55:47 adunstan Exp $
+ *	  $PostgreSQL: pgsql/src/pl/plperl/plperl.c,v 1.136.2.2 2009/06/05 20:32:15 adunstan Exp $
  *
  **********************************************************************/
 
@@ -386,6 +386,10 @@ plperl_init_interp(void)
 		"", "-e", PERLBOOT
 	};
 
+	int nargs = 3;
+
+	char *dummy_perl_env[1] = { NULL }; 
+
 #ifdef WIN32
 
 	/*
@@ -428,6 +432,20 @@ plperl_init_interp(void)
 	save_time = loc ? pstrdup(loc) : NULL;
 #endif
 
+	/****
+	 * The perl API docs state that PERL_SYS_INIT3 should be called before
+	 * allocating interprters. Unfortunately, on some platforms this fails
+	 * in the Perl_do_taint() routine, which is called when the platform is
+	 * using the system's malloc() instead of perl's own. Other platforms,
+	 * notably Windows, fail if PERL_SYS_INIT3 is not called. So we call it
+	 * if it's available, unless perl is using the system malloc(), which is
+	 * true when MYMALLOC is set.
+	 */
+#if defined(PERL_SYS_INIT3) && !defined(MYMALLOC)
+	/* only call this the first time through, as per perlembed man page */
+	if (interp_state == INTERP_NONE)
+		PERL_SYS_INIT3(&nargs, (char ***) &embedding, (char***)&dummy_perl_env);
+#endif
 
 	plperl_held_interp = perl_alloc();
 	if (!plperl_held_interp)
@@ -435,7 +453,7 @@ plperl_init_interp(void)
 
 	perl_construct(plperl_held_interp);
 	perl_parse(plperl_held_interp, plperl_init_shared_libs,
-			   3, embedding, NULL);
+			   nargs, embedding, NULL);
 	perl_run(plperl_held_interp);
 
 	if (interp_state == INTERP_NONE)
