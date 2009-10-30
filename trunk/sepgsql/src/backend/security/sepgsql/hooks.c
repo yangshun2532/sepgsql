606e7ab1446aea4669abc38f4e5b1d84bfedf819
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-10-30 15:11:04 +0000

    add source code comments

--- trunk/sepgsql/src/backend/security/sepgsql/hooks.c
+++ trunk/sepgsql/src/backend/security/sepgsql/hooks.c
@@ -14,12 +14,23 @@
 #include "catalog/pg_namespace.h"
 #include "utils/syscache.h"
 
+/* ---- static declarations ---- */
+static bool sepgsql_database_common(Oid datOid, uint32 required, bool abort);
+static bool sepgsql_schema_common(Oid nspOid, uint32 required, bool abort);
+static bool sepgsql_relation_common(Oid relOid, uint32 required, bool abort);
+
 /************************************************************
  *
  * Pg_database related security hooks
  *
  ************************************************************/
 
+/*
+ * sepgsql_database_common
+ *
+ * A helper function to check required permissions on a pair of the client
+ * and the given database.
+ */
 static bool
 sepgsql_database_common(Oid datOid, uint32 required, bool abort)
 {
@@ -56,12 +67,35 @@ sepgsql_database_common(Oid datOid, uint32 required, bool abort)
 	return rc;
 }
 
+/*
+ * sepgsql_database_create
+ *
+ * It checks client's privilege to create a new database, and returns
+ * a security context (in raw-format) to be assigned on. 
+ * If violated, it raises an error.
+ *
+ * This hook should be called on the routine to handle CREATE DATABASE
+ * statement, then the caller has to assign the returned context on the
+ * new database.
+ *
+ * If no explicit security context is given, it compute a default security
+ * context of the new database. Then, it checks db_database:{create}
+ * permission on the pair of client's context and the default context.
+ * If an explicit one is given, it shall be applied instead of the
+ * default one after the sanity and validation checks.
+ * 
+ * datName : name of the new database
+ * datLabel : an explicit security context, or NULL
+ */
 Datum
 sepgsql_database_create(const char *datName, DefElem *datLabel)
 {
 	if (!sepgsql_is_enabled())
 	{
-		/* we don't allow SECURITY_CONTEXT option with SE-PgSQL disabled */
+		/*
+		 * We don't allow SECURITY_CONTEXT option without
+		 * SE-PgSQL enaled.
+		 */
 		if (datLabel)
 			ereport(ERROR,
 					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
@@ -71,9 +105,10 @@ sepgsql_database_create(const char *datName, DefElem *datLabel)
 	}
 
 	/*
-	 * If client gives an explicit security context, 
-	 *
-	 *
+	 * If an explicit security context is given, we apply it with
+	 * translation into raw format (when mcstrans is enabled).
+	 * Otherwise, we compute a default security context of the new
+	 * database.
 	 */
 	if (datLabel)
 		context = sepgsql_mcstrans_in(strVal(datLabel->arg));
@@ -90,9 +125,25 @@ sepgsql_database_create(const char *datName, DefElem *datLabel)
 						  SEPG_DB_DATABASE__CREATE,
 						  datName, true);
 
+	/*
+	 * The checked security context should be returned to caller.
+	 * Caller has to assign it on the new database.
+	 */
 	return CStringGetTextDatum(context);
 }
 
+/*
+ * sepgsql_database_alter
+ *
+ * It checks client's privilege to alter a certain database.
+ * If violated, it raises an error.
+ *
+ * This hook should be called on the routine to handle ALTER DATABASE
+ * statement. It modifies metadata of the database, so we need to check
+ * db_database:{setattr} permission on a pair of client and the database.
+ *
+ * datOid : OID of the database to be altered
+ */
 void
 sepgsql_database_alter(Oid datOid)
 {
@@ -102,6 +153,18 @@ sepgsql_database_alter(Oid datOid)
 	sepgsql_database_common(datOid, SEPG_DB_DATABASE__SETATTR, true);
 }
 
+/*
+ * sepgsql_database_drop
+ *
+ * It checks client's privilege to drop a certain database.
+ * If violated, it raises an error.
+ *
+ * This hook should be called on the routine to handle DROP DATABASE
+ * statement. It drops the database itself, so we need to check
+ * db_database:{drop} permission on a pair of client and the database.
+ *
+ * datOid : OID of the database to be dropped
+ */
 void
 sepgsql_database_drop(Oid datOid)
 {
@@ -111,6 +174,26 @@ sepgsql_database_drop(Oid datOid)
 	sepgsql_database_common(datOid, SEPG_DB_DATABASE__DROP, true);
 }
 
+/*
+ * sepgsql_database_relabel
+ *
+ * It checks client's privilege to change security context of a certain
+ * database. If violated, it raises an error.
+ *
+ * This hook should be called on the routine to handle ALTER DATABASE
+ * statement with SECURITY_CONTEXT option. It modifies a special metadata,
+ * so it requires two more permissions, not only db_database:{setattr}.
+ * It checks db_database:{relabelfrom} permission on the older security
+ * context of the database, and db_database:{relabelto} on the newer
+ * security context to be assigned on.
+ *
+ * This hook also applies validation check on the given security context
+ * and translates it into raw-format. The caller has to assign the returned
+ * security context on the target database correctly.
+ *
+ * datOid : OID of the target database
+ * datLabel : An explicit security context to be assigned on
+ */
 Datum
 sepgsql_database_relabel(Oid datOid, DefElem *datLabel)
 {
@@ -140,6 +223,18 @@ sepgsql_database_relabel(Oid datOid, DefElem *datLabel)
 	return CStringGetTextDatum(context);
 }
 
+/*
+ * sepgsql_database_grant
+ *
+ * It checks client's privilege to grant/revoke permissions on a certain
+ * database. If violated, it raises an error.
+ *
+ * This hook should be called on the routine to handle GRANT/REVOKE statement
+ * on databases. It also modifies metadata of the database, so we need to
+ * check db_database:{setattr} permission on a pair of client and the database.
+ *
+ * datOid : OID of the database to be altered
+ */
 void
 sepgsql_database_grant(Oid datOid)
 {
@@ -149,6 +244,14 @@ sepgsql_database_grant(Oid datOid)
 	sepgsql_database_common(datOid, SEPG_DB_DATABASE__SETATTR, true);
 }
 
+/*
+ * sepgsql_database_access
+ *
+ * It checks client's privilege to access to the selected database just after
+ * ACL_CONNECT checks in the default PG model.
+ *
+ * datOid : OID of the database to be accessed
+ */
 void
 sepgsql_database_access(Oid datOid)
 {
@@ -158,6 +261,20 @@ sepgsql_database_access(Oid datOid)
 	sepgsql_database_common(datOid, SEPG_DB_DATABASE__ACCESS, true);
 }
 
+/*
+ * sepgsql_database_superuser
+ *
+ * It checks client's privilege to perform as a database superuser on
+ * a certain database. If violated, it returns false. In this case,
+ * the caller has to prevent caller performs as a database superuser.
+ * Otherwise, it follows the configuration in the default PG model.
+ *
+ * This hook should be called on the test whether user has database
+ * superuser privilege, or not. It checks db_database:{superuser}
+ * permission on the pair of client and database.
+ *
+ * datOid : OID of the database (maybe equal to MyDatabaseId)
+ */
 bool
 sepgsql_database_superuser(Oid datOid)
 {
@@ -167,6 +284,15 @@ sepgsql_database_superuser(Oid datOid)
 	return sepgsql_database_common(datOid, SEPG_DB_DATABASE__SUPERUSER, false);
 }
 
+/*
+ * sepgsql_database_load_module
+ *
+ * It checks database's capability to load a binary module into the
+ *
+ *
+ *
+ *
+ */
 void
 sepgsql_database_load_module(Oid datOid, const char *filename)
 {
@@ -223,6 +349,13 @@ sepgsql_database_load_module(Oid datOid, const char *filename)
  * Pg_namespace related security hooks
  *
  ************************************************************/
+
+/*
+ * sepgsql_schema_common
+ *
+ * A helper function to check required permissions on a pair of the client
+ * and the given schema
+ */
 static bool
 sepgsql_schema_common(Oid nspOid, uint32 required, bool abort)
 {
@@ -259,6 +392,27 @@ sepgsql_schema_common(Oid nspOid, uint32 required, bool abort)
 	return rc;
 }
 
+/*
+ * sepgsql_schema_create
+ *
+ * It checks client's privilege to create a new schema, and returns
+ * a security context (in raw-format) to be assigned on. 
+ * If violated, it raises an error.
+ *
+ * This hook should be called on the routine to handle CREATE SCHEMA or
+ * to create a temporary schema, then the caller has to assign the returned
+ * context on the new schema.
+ *
+ * If no explicit security context is given, it compute a default security
+ * context of the new schema. Then, it checks db_schema:{create} permission
+ * on the pair of client's context and the default context.
+ * If an explicit one is given, it shall be applied instead of the default
+ * one after the sanity and validation checks.
+ * 
+ * nspName : name of the new schema
+ * isTemp : true, if creation of the temporary schema
+ * nspLabel : an explicit security context, or NULL
+ */
 Datum
 sepgsql_schema_create(const char *nspName, bool isTemp, DefElem *nspLabel)
 {
@@ -287,6 +441,18 @@ sepgsql_schema_create(const char *nspName, bool isTemp, DefElem *nspLabel)
 	return CStringGetTextDatum(context);
 }
 
+/*
+ * sepgsql_database_alter
+ *
+ * It checks client's privilege to alter a certain schema.
+ * If violated, it raises an error.
+ *
+ * This hook should be called on the routine to handle ALTER SCHEMA
+ * statement. It modifies metadata of the schema, so we need to check
+ * db_schema:{setattr} permission on a pair of client and the schema.
+ *
+ * nspOid : OID of the schema to be altered
+ */
 void
 sepgsql_schema_alter(Oid nspOid)
 {
@@ -296,6 +462,22 @@ sepgsql_schema_alter(Oid nspOid)
 	sepgsql_schema_common(nspOid, SEPG_DB_SCHEMA__SETATTR, true);
 }
 
+/*
+ * sepgsql_schema_drop
+ *
+ * It checks client's privilege to drop a certain schema.
+ * If violated, it raises an error.
+ *
+ * This hook should be called on the routine to handle DROP SCHEMA
+ * statement. Note that we don't need to check anything on cleaning up
+ * temporary schema after the session closed, because it is a purely
+ * internal process.
+ *
+ * It drops an existing schema, so we need to check db_schema:{setattr}
+ * permission on a pair of client and the schema.
+ *
+ * nspOid : OID of the schema to be dropped
+ */
 void
 sepgsql_schema_drop(Oid nspOid)
 {
@@ -305,6 +487,26 @@ sepgsql_schema_drop(Oid nspOid)
 	sepgsql_schema_common(nspOid, SEPG_DB_SCHEMA__DROP, true);
 }
 
+/*
+ * sepgsql_schema_relabel
+ *
+ * It checks client's privilege to change security context of a certain
+ * schema. If violated, it raises an error.
+ *
+ * This hook should be called on the routine to handle ALTER SCHEMA
+ * statement with SECURITY_CONTEXT option. It modifies a special metadata,
+ * so it requires two more permissions, not only db_schema:{setattr}.
+ * It checks db_schema:{relabelfrom} permission on the older security
+ * context of the schema, and db_schema:{relabelto} on the newer one
+ * to be assigned on.
+ *
+ * This hook also applies validation check on the given security context
+ * and translates it into raw-format. The caller has to assign the returned
+ * security context on the target schema correctly.
+ *
+ * nspOid : OID of the target schema
+ * nspLabel : An explicit security context to be assigned on
+ */
 Datum
 sepgsql_schema_relabel(Oid nspOid, DefElem *nspLabel)
 {
@@ -334,6 +536,18 @@ sepgsql_schema_relabel(Oid nspOid, DefElem *nspLabel)
 	return CStringGetTextDatum(context);
 }
 
+/*
+ * sepgsql_database_grant
+ *
+ * It checks client's privilege to grant/revoke permissions on a certain
+ * database. If violated, it raises an error.
+ *
+ * This hook should be called on the routine to handle GRANT/REVOKE statement
+ * on databases. It also modifies metadata of the database, so we need to
+ * check db_database:{setattr} permission on a pair of client and the database.
+ *
+ * datOid : OID of the database to be altered
+ */
 void
 sepgsql_schema_grant(Oid nspOid)
 {
@@ -343,6 +557,21 @@ sepgsql_schema_grant(Oid nspOid)
 	sepgsql_schema_common(nspOid, SEPG_DB_SCHEMA__SETATTR, true);
 }
 
+/*
+ * sepgsql_schema_search
+ *
+ * It checks client's privilege to resolve lookup database objects within
+ * a certain schema. If violated, it raised an error or returns false,
+ * depending on the second argument (abort).
+ * If it returned false, the caller has to drop the given schema from
+ * schema search path correctly.
+ *
+ * This hook should be called on (re)computing schema search path or
+ * looking up a database object with an explicit namespace.
+ *
+ * nspOid : OID of the schema to be searched
+ * abort : True, if caller want to raise an error on access violation.
+ */
 bool
 sepgsql_schema_search(Oid nspOid, bool abort)
 {
@@ -394,6 +623,17 @@ Datum
 sepgsql_relation_relabel(Oid relOid, DefElem *relLabel)
 {}
 
+/*
+ * sepgsql_relation_truncate
+ *
+ * It checks client's privilege to truncate contents of the given table.
+ * If violated, it raises an error.
+ *
+ * This hook should be called on truncate_check_rel() which also checks
+ * permission in the default PG model.
+ *
+ * rel : Relation object to be truncated
+ */
 void
 sepgsql_relation_truncate(Relation rel)
 {}
@@ -412,10 +652,6 @@ sepgsql_index_create(Oid relOid, Oid nspOid)
 {
 }
 
-
-
-
-
 /************************************************************
  *
  * Pg_attribute related security hooks
@@ -430,6 +666,32 @@ sepgsql_index_create(Oid relOid, Oid nspOid)
  * Misc objects related security hooks
  *
  ************************************************************/
+void
+sepgsql_object_comment(Oid relOid, Oid objId, int32 subId)
+{
+	switch (relOid)
+	{
+	case DatabaseRelationId:
+		sepgsql_database_common(objId, SEPG_DB_DATABASE__SETATTR, true);
+		break;
+
+	case NamespaceRelationId:
+		sepgsql_schema_common(objId, SEPG_DB_SCHEMA__SETATTR, true);
+		break;
+
+	case RelationRelationId:
+		if (subId == 0)
+			sepgsql_relation_common(objId, SEPG_DB_TABLE__SETATTR, true);
+		else
+			sepgsql_attribute_common();
+		break;
+
+	default:
+		/* do nothing */
+		break;
+	}
+}
+
 void
 sepgsql_object_drop(ObjectAddress *object)
 {
@@ -453,6 +715,3 @@ sepgsql_object_drop(ObjectAddress *object)
 		break;
 	}
 }
-
-
-
