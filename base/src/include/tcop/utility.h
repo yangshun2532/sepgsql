c900f07353689f01fd187724c3d457c9527767d6
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-12-04 04:55:38 +0000

    CVS pull -r HEAD at Mon Dec  7 05:02:09 JST 2009

--- base/src/include/tcop/utility.h
+++ base/src/include/tcop/utility.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/tcop/utility.h,v 1.35 2009/01/01 17:24:01 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/tcop/utility.h,v 1.37 2009/12/01 02:31:13 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
