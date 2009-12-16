50aa022e49718c78f8e19c7bff5a651dd8802efe
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2009-12-16 08:19:22 +0000

    just a skelton

---
+++ branches/pgsql-8.4.x/sepgsql-new/src/backend/security/pgace.c
@@ -0,0 +1,18 @@
+/*
+ * security/pgace.c
+ *
+ * PostgreSQL Access Control Extension
+ *
+ * Copyright (C) 2009 NEC Corporation, KaiGai Kohei <kaigai@ak.jp.nec.com>
+ */
+
+#include "postgres.h"
+
+#include "security/pgace.h"
+#include "security/sepgsql.h"
+
+
+
+
+
+
