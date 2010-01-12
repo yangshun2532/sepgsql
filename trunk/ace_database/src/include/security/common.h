282886edde661d64febbfa68c8b37b40b89b2846
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2010-01-12 06:42:44 +0000

    fix header declaration style in ace_database

--- trunk/ace_database/src/include/security/common.h
+++ trunk/ace_database/src/include/security/common.h
@@ -12,30 +12,19 @@
 #include "utils/acl.h"
 
 /*
- * database.c - hooks related to databases
+ * database.c - security checks related to databases
  */
-extern void
-check_database_create(const char *datName, Oid srcDatOid,
-					  Oid datOwner, Oid datTblspc);
-extern void
-check_database_alter(Oid datOid);
-extern void
-check_database_alter_rename(Oid datOid, const char *newName);
-extern void
-check_database_alter_owner(Oid datOid, Oid newOwner);
-extern void
-check_database_alter_tablespace(Oid datOid, Oid newTblspc);
-extern void
-check_database_drop(Oid datOid, bool cascade);
-extern void
-check_database_getattr(Oid datOid);
-extern void
-check_database_grant(Oid datOid);
-extern void
-check_database_comment(Oid datOid);
-extern void
-check_database_connect(Oid datOid);
-extern void
-check_database_reindex(Oid datOid);
+extern void check_database_create(const char *datName, Oid srcDatOid,
+								  Oid datOwner, Oid datTblspc);
+extern void check_database_alter(Oid datOid);
+extern void check_database_alter_rename(Oid datOid, const char *newName);
+extern void check_database_alter_owner(Oid datOid, Oid newOwner);
+extern void check_database_alter_tablespace(Oid datOid, Oid newTblspc);
+extern void check_database_drop(Oid datOid, bool cascade);
+extern void check_database_getattr(Oid datOid);
+extern void check_database_grant(Oid datOid);
+extern void check_database_comment(Oid datOid);
+extern void check_database_connect(Oid datOid);
+extern void check_database_reindex(Oid datOid);
 
 #endif	/* SECURITY_COMMON_H */
