0c669779dd68cfdc9326c0926ac477dd01eb473c
KaiGai.Kohei <KaiGai.Kohei@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2010-01-20 09:23:35 +0000

    git script for v8.5

---
+++ scripts/gitpull-pgsql-8.5.x.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+GIT_BIN=`which git`
+GIT_DIR="${HOME}/repo/sepgsql"
+PGSQL_GIT="git://git.postgresql.org/git/postgresql.git"
+UPSTREAM="master"
+LOCAL_GIT="git://git.postgresql.org/git/users/kaigai/sepgsql.git"
+BRANCHES="master devel/ace_database devel/ace_schema devel/ace_relation_pre"
+#================================================================
+
+cd ${GIT_DIR}
+
+for tree in ${BRANCHES}
+do
+    echo "CHECK: ${tree} branch"
+    ${GIT_BIN} branch -lr | grep -q "origin/${tree}" || exit 1
+    ${GIT_BIN} branch -l | grep -q "${tree}" || exit 1
+done
+
+ORIG_BRANCH=`${GIT_BIN} branch -l | grep '^*' | awk '{print $2}'`
+
+REMOTE_GIT=${PGSQL_GIT}
+for tree in ${BRANCHES}
+do
+    ${GIT_BIN} checkout ${tree} || exit 1
+    ${GIT_BIN} pull ${REMOTE_GIT} ${UPSTREAM} || exit 1
+    ${GIT_BIN} push
+
+    UPSTREAM=${tree}
+    REMOTE_GIT=${LOCAL_GIT}
+done
+
+${GIT_BIN} checkout ${ORIG_BRANCH}
