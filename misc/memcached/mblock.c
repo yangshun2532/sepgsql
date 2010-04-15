4b895df1367f74adc163838118446da5aa1b8c09
kaigai@ak.jp.nec.com <kaigai@ak.jp.nec.com@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2010-04-15 01:28:28 +0000

    fix various kind of initial bugs

--- misc/memcached/mblock.c
+++ misc/memcached/mblock.c
@@ -40,7 +40,7 @@ typedef struct {
 typedef struct {
 	uint32_t	magic;
 	uint32_t	flags;
-	uint64_t	total_size;
+	uint64_t	segment_size;	/* segment size of the block */
 
 	mblock_list	active_list[MBLOCK_MAX_BITS + 1];
 	mblock_list	free_list[MBLOCK_MAX_BITS + 1];
@@ -81,11 +81,10 @@ mblock_list_init(mblock_head *mhead, mblock_list *list)
 static inline void
 mblock_list_add(mblock_head *mhead, mblock_list *base, mblock_list *list)
 {
-	mblock_list	   *plist = offset_to_addr(mhead, base->prev);
 	mblock_list	   *nlist = offset_to_addr(mhead, base->next);
 
-	plist->next = addr_to_offset(mhead, list);
-	list->prev = addr_to_offset(mhead, plist);
+	base->next = addr_to_offset(mhead, list);
+	list->prev = addr_to_offset(mhead, base);
 	list->next = addr_to_offset(mhead, nlist);
 	nlist->prev = addr_to_offset(mhead, list);
 }
@@ -206,6 +205,7 @@ mblock_free(void *handle, void *ptr)
 
 	/* Detach from the active list */
 	mblock_list_del(mhead, &mchunk->list);
+	mchunk->flags &= ~MCHUNK_FLAGS_ACTIVE;
 	mhead->num_active[index]--;
 
 	while (index < MBLOCK_MAX_BITS)
@@ -249,17 +249,28 @@ mblock_stat(void *handle, char *buffer, size_t buflen)
 	mblock_head	   *mhead = handle;
 	uint64_t		total_active = 0;
 	uint64_t		total_free = 0;
-	int				i, ofs = 0;
+	int				index, ofs = 0;
 
-	ofs += snprintf(buffer, buflen, "total_size: %" PRIu64 "\n",
-					mhead->total_size);
-	for (i = MBLOCK_MIN_BITS; i <= MBLOCK_MAX_BITS; i++)
+	ofs += snprintf(buffer, buflen, "segment_size: %" PRIu64 "\n",
+					mhead->segment_size);
+	for (index = MBLOCK_MIN_BITS; index <= MBLOCK_MAX_BITS; index++)
 	{
+		if ((1<<index) < 1024)
+			ofs += snprintf(buffer + ofs, buflen - ofs,
+							"% 4ubyte: ", (1<<index));
+		else if ((1<<index) < 1024 * 1024)
+			ofs += snprintf(buffer + ofs, buflen - ofs,
+							"% 6uKB: ", (1<<(index - 10)));
+		else
+			ofs += snprintf(buffer + ofs, buflen - ofs,
+							"% 6uMB: ", (1<<(index - 20)));
 		ofs += snprintf(buffer + ofs, buflen - ofs,
-						"class %02d: %lu of used, %lu of free\n",
-						mhead->num_active[i], mhead->num_free[i]);
-		total_active += mhead->num_active[i] * (1 << i);
-		total_free += mhead->num_free[i] * (1 << i);
+						"%lu of used, %lu of free\n",
+						mhead->num_active[index],
+						mhead->num_free[index]);
+
+		total_active += mhead->num_active[index] * (1 << index);
+		total_free += mhead->num_free[index] * (1 << index);
 	}
 
 	ofs += snprintf(buffer + ofs, buflen - ofs,
@@ -271,8 +282,8 @@ mblock_stat(void *handle, char *buffer, size_t buflen)
 }
 
 void *
-mblock_init(int fdesc, size_t total_size,
-			bool (*callback_mchunk)(void *data))
+mblock_init(int fdesc, size_t segment_size,
+			bool (*callback_mchunk)(void *data, size_t size))
 {
 	mblock_head	   *mhead;
 	mblock_chunk   *mchunk;
@@ -280,9 +291,9 @@ mblock_init(int fdesc, size_t total_size,
 	int				index;
 	int				count;
 
-	mhead = (mblock_head *) mmap(NULL, total_size,
+	mhead = (mblock_head *) mmap(NULL, segment_size,
 								 PROT_READ | PROT_WRITE,
-								 fdesc < 0 ? MAP_ANONYMOUS : MAP_SHARED,
+								 fdesc < 0 ? MAP_ANONYMOUS | MAP_PRIVATE : MAP_SHARED,
 								 fdesc, 0);
 	if (mhead == MAP_FAILED)
 		return NULL;
@@ -292,6 +303,7 @@ mblock_init(int fdesc, size_t total_size,
 		/* construct memory block */
 		mhead->magic = MBLOCK_HEAD_MAGIC;
 		mhead->flags = 0;
+		mhead->segment_size = segment_size;
 
 		pthread_mutex_init(&mhead->lock, NULL);
 
@@ -308,18 +320,18 @@ mblock_init(int fdesc, size_t total_size,
 			 offset < sizeof(mblock_head);
 			 offset <<= 1);
 
-		while (total_size - offset >= MBLOCK_MIN_SIZE)
+		while (segment_size - offset >= MBLOCK_MIN_SIZE)
 		{
 			/* choose an appropriate chunk class */
 			index = ffsll(offset) - 1;
-			assert(index < MBLOCK_MIN_BITS);
+			assert(index >= MBLOCK_MIN_BITS);
 
 			/* truncate to maximum size */
 			if (index > MBLOCK_MAX_BITS)
 				index = MBLOCK_MAX_BITS;
 
 			/* if (offset + chunk_size) over the tail, truncate it */
-			while (total_size > offset + (1 << index))
+			while (segment_size < offset + (1 << index))
 				index--;
 
 			if (index < MBLOCK_MIN_BITS)
@@ -334,7 +346,6 @@ mblock_init(int fdesc, size_t total_size,
 
 			offset += (1 << index);
 		}
-		mhead->total_size = offset;
 	}
 	else
 	{
@@ -342,11 +353,15 @@ mblock_init(int fdesc, size_t total_size,
 		struct timespec		timeout = { .tv_sec = 5, .tv_nsec = 0 };
 		uint32_t	num_free[MBLOCK_MAX_BITS + 1];
 		uint32_t	num_active[MBLOCK_MAX_BITS + 1];
-		uint64_t	total_free;
-		uint64_t	total_active;
+		uint64_t	total_free = 0;
+		uint64_t	total_active = 0;
 
-		/* try to lock this mapped file */
-		if (pthread_mutex_timedlock(&mhead->lock, &timeout) != 0)
+		/*
+		 * XXX - we need check to prevent concurrent file using
+		 */
+		pthread_mutex_init(&mhead->lock, NULL);
+
+		if (mhead->segment_size != segment_size)
 			goto error;
 
 		memset(num_free, 0, sizeof(num_free));
@@ -354,6 +369,8 @@ mblock_init(int fdesc, size_t total_size,
 
 		for (index = 0; index <= MBLOCK_MAX_BITS; index++)
 		{
+			size_t	size = (1 << index) - offset_of(mblock_chunk, data);
+
 			offset = mhead->free_list[index].next;
 			while (offset_to_addr(mhead, offset) != &mhead->free_list[index])
 			{
@@ -380,30 +397,109 @@ mblock_init(int fdesc, size_t total_size,
 				if (!mchunk_active(mchunk))
 					goto error;
 
+				if (callback_mchunk && !callback_mchunk(mchunk->data, size))
+					goto error;
+
 				total_active += (1 << index);
 				num_active[index]++;
 
 				offset = mchunk_next(mchunk);
 			}
-
 			if (mhead->num_free[index] != num_free[index] ||
-				mhead->num_active[index] != num_active[index] ||
-				mhead->total_size != (total_free + total_active))
+				mhead->num_active[index] != num_active[index])
 				goto error;
 		}
-		
-		/* unlock mapped file */
-		pthread_mutex_unlock(&mhead->lock);
 	}
 	return (void *)mhead;
 
 error:
-	munmap(mhead, total_size);
+	munmap(mhead, segment_size);
 	return NULL;
+}
+
+#if 1
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+static int my_index = 0;
+static void *my_ptr[1024];
+
+static bool
+my_callback(void *data, size_t size)
+{
+	printf("%s called on %p (size=%d)\n", __FUNCTION__, data, size);
+
+	my_ptr[my_index++] = data;
+
+	return true;
 }
 
 int main(int argc, const char *argv[])
 {
+	int		i, fd = -1;
+	size_t	length = 1024 * 1024;
+	void   *handle;
+	char	buffer[1024];
+	void   *a, *b, *c;
+
+	if (argc > 2)
+	{
+		length = atol(argv[2]);
+		if (length < 1024 * 1024)
+		{
+			fprintf(stderr, "file length too short %u\n", length);
+			return 1;
+		}
+	}
+
+	if (argc > 1)
+	{
+		fd = open(argv[1], O_RDWR | O_CREAT);
+		if (fd < 0)
+		{
+			fprintf(stderr, "failed to open %s\n", argv[1]);
+			return 1;
+		}
+	}
+
+	handle = mblock_init(fd, length, my_callback);
+	if (!handle)
+	{
+		fprintf(stderr, "failed to init mblock\n");
+		return 1;
+	}
+
+	mblock_stat(handle, buffer, sizeof(buffer));
+	puts(buffer);
+
+	a = mblock_alloc(handle, 1);
+
+	mblock_stat(handle, buffer, sizeof(buffer));
+	puts(buffer);
+
+	b = mblock_alloc(handle, 385);
+
+	mblock_stat(handle, buffer, sizeof(buffer));
+	puts(buffer);
+
+	c = mblock_alloc(handle, 3);
+
+	mblock_stat(handle, buffer, sizeof(buffer));
+	puts(buffer);
+
+	mblock_free(handle, a);
+	mblock_free(handle, b);
+	mblock_free(handle, c);
+
+	printf("mokeke\n");
+
+	while (my_index > 0)
+		mblock_free(handle, my_ptr[--my_index]);
+
+	mblock_stat(handle, buffer, sizeof(buffer));
+	puts(buffer);
 
 	return 0;
 }
+#endif
