2a94e8139fdc2e25f4a6ad2ee86bb368afbf04a8
kaigai@ak.jp.nec.com <kaigai@ak.jp.nec.com@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2010-07-01 09:25:51 +0000

    a compilable mblock.c

--- trunk/memcached/memcached_selinux.h
+++ trunk/memcached/memcached_selinux.h
@@ -8,8 +8,56 @@
 #ifndef SELINUX_ENGINE_H
 #define SELINUX_ENGINE_H
 
-#define TAG_SELINUX_LABEL		0x03
-#define TAG_SELINUX_ITEM		0x04
+/*
+ * mlist_t - dual linked list on the memory block
+ */
+typedef struct {
+	uint64_t	prev;
+	uint64_t	next;
+} mlist_t;
+
+/*
+ * mchunk_t
+ *
+ * memory block chunk for various kind of data block
+ */
+#define MCHUNK_TAG_FREE		0
+#define MCHUNK_TAG_ITEM		1
+#define MCHUNK_TAG_BTREE	2
+#define MCHUNK_TAG_LABEL	3
+
+typedef struct {
+	uint16_t	magic;
+	uint8_t		mclass;
+	uint8_t		tag;
+	union {
+		/* MCHUNK_TAG_FREE */
+		struct {
+			mlist_t		list;
+		} free;
+		/* MCHUNK_TAG_ITEM */
+		struct {
+			uint16_t	flags;
+			uint16_t	keylen;
+			uint32_t	datalen;
+			uint32_t	exptime;
+			uint8_t		data[0];
+		} item;
+		/* MCHUNK_TAG_BTREE */
+		struct {
+#define MBTREE_NUM_KEYS		7
+			uint64_t	parent;
+			uint8_t		is_leaf;
+			uint32_t	keys[MBTREE_NUM_KEYS];
+			uint64_t	items[MBTREE_NUM_KEYS + 1];
+		} btree;
+		/* MCHUNK_TAG_LABEL */
+		struct {
+			uint32_t	refcount;
+			uint8_t		label[0];
+		} label;
+	};
+} mchunk_t;
 
 /*
  * mbtree.c - mmap based B-plus tree index
@@ -25,41 +73,106 @@ typedef struct
 	uint64_t	item;
 } mbtree_scan;
 
-extern bool mbtree_lookup(void *handle, uint32_t key, mbtree_scan *scan);
-extern void mbtree_dump(void *handle);
-extern bool mbtree_insert(void *handle, uint32_t key, uint64_t item);
-extern bool mbtree_delete(void *handle, uint32_t key, uint64_t item);
+extern bool  mbtree_lookup(void *handle, uint32_t key, mbtree_scan *scan);
+extern void  mbtree_dump(void *handle);
+extern bool  mbtree_insert(void *handle, uint32_t key, uint64_t item);
+extern bool  mbtree_delete(void *handle, uint32_t key, uint64_t item);
 extern void *mbtree_init(int fdesc, size_t block_size);
-
-/*
- * mhash.c - mmap based hash index
- */
-typedef struct
-{
-	/* internal pointer, don't touch! */
-	uint64_t	entry;
-
-	/* fetched key&item pair */
-	uint32_t	key;
-	uint64_t	item;
-} mhash_scan;
-
-extern bool mhash_lookup(void *handle, uint32_t key, mhash_scan *scan);
-extern void mhash_dump(void *handle);
-extern bool mhash_insert(void *handle, uint32_t key, uint64_t item);
-extern bool mhash_delete(void *handle, uint32_t key, uint64_t item);
-extern void *mhash_init(int fdesc, size_t block_size);
 
 /*
  * mblock.c - memory block management
  */
-extern uint64_t mblock_addr_to_offset(void *handle, void *addr);
-extern void *mblock_offset_to_addr(void *handle, uint64_t offset);
-extern void *mblock_alloc(void *handle, size_t size);
-extern void  mblock_free(void *handle, void *ptr);
-extern void  mblock_reset(void *handle);
-extern void  mblock_dump(void *handle);
-extern void *mblock_init(int fdesc, size_t block_size, size_t super_size);
-extern void  mblock_unmap(void *handle);
+extern mchunk_t *mblock_alloc(void *handle, uint8_t tag, size_t size);
+extern void      mblock_free(void *handle, mchunk_t *mchunk);
+extern void      mblock_dump(void *handle);
+extern void      mblock_reset(void *handle);
+extern void     *mblock_map(int fdesc, size_t block_size, size_t super_size);
+extern void      mblock_unmap(void *handle);
+
+/*
+ * ffs64 - returns first (smallest) bit of the value
+ */
+static inline int
+ffs64(uint64_t value)
+{
+	int		ret = 1;
+
+	if (!value)
+		return 0;
+	if (!(value & 0xffffffff))
+	{
+		value >>= 32;
+		ret += 32;
+	}
+	if (!(value & 0x0000ffff))
+	{
+		value >>= 16;
+		ret += 16;
+	}
+	if (!(value & 0x000000ff))
+	{
+		value >>= 8;
+		ret += 8;
+	}
+	if (!(value & 0x0000000f))
+	{
+		value >>= 4;
+		ret += 4;
+	}
+	if (!(value & 0x00000003))
+	{
+		value >>= 2;
+		ret += 2;
+	}
+	if (!(value & 0x00000001))
+	{
+		value >>= 1;
+		ret += 1;
+	}
+	return ret;
+}
+
+/*
+ * fls64 - returns last (biggest) bit of the value
+ */
+static inline int
+fls64(uint64_t value)
+{
+	int		ret = 1;
+
+	if (!value)
+		return 0;
+	if (value & 0xffffffff00000000)
+	{
+		value >>= 32;
+		ret += 32;
+	}
+	if (value & 0xffff0000)
+	{
+		value >>= 16;
+		ret += 16;
+	}
+	if (value & 0xff00)
+	{
+		value >>= 8;
+		ret += 8;
+	}
+	if (value & 0xf0)
+	{
+		value >>= 4;
+		ret += 4;
+	}
+	if (value & 0xc)
+	{
+		value >>= 2;
+		ret += 2;
+	}
+	if (value & 0x2)
+	{
+		value >>= 1;
+		ret += 1;
+	}
+	return ret;
+}
 
 #endif
