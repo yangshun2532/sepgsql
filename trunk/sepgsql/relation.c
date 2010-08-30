8adc1616bb39557593fd916cb565581d09941e61
kaigai@ak.jp.nec.com <kaigai@ak.jp.nec.com@b76cc0ed-a51f-0410-b87d-45c42ba30643>
2010-08-30 06:25:37 +0000

    add hook for create table

--- trunk/sepgsql/relation.c
+++ trunk/sepgsql/relation.c
@@ -11,10 +11,14 @@
 #include "postgres.h"
 
 #include "access/sysattr.h"
+#include "access/tupdesc.h"
 #include "catalog/catalog.h"
+#include "catalog/heap.h"
 #include "catalog/pg_attribute.h"
 #include "catalog/pg_class.h"
 #include "catalog/pg_inherits_fn.h"
+#include "commands/seclabel.h"
+#include "commands/tablecmds.h"
 #include "executor/executor.h"
 #include "nodes/bitmapset.h"
 #include "utils/lsyscache.h"
@@ -332,6 +336,95 @@ sepgsql_relation_privileges(List *rangeTabls, bool abort)
 	return true;
 }
 
+/*****/
+static List *
+sepgsql_relation_create(const char *relName,
+						Oid namespaceId,
+						Oid tablespaceId,
+						char relkind,
+						TupleDesc tupdesc,
+						bool createAs)
+{
+	SecLabelItem   *sl;
+	char		   *tcontext = "system_u:object_r:sepgsql_table_t:s0";
+	uint16			tclass;
+	uint32			required;
+	char			auname[NAMEDATALEN * 2 + 10];
+	int				index;
+	List		   *seclabels = NIL;
+
+	if (relkind == RELKIND_RELATION)
+		tclass = SEPG_CLASS_DB_TABLE;
+	else if (relkind == RELKIND_SEQUENCE)
+		tclass = SEPG_CLASS_DB_SEQUENCE;
+	else if (relkind == RELKIND_VIEW)
+		tclass = SEPG_CLASS_DB_TUPLE;
+	else
+		return NIL;
+
+	required = SEPG_DB_TABLE__CREATE;
+	if (createAs)
+		required |= SEPG_DB_TABLE__INSERT;
+
+	/* permission on the relation */
+	sepgsql_compute_perms(sepgsql_get_client_label(),
+						  tcontext,
+						  tclass,
+						  required,
+						  relName,
+						  true);
+
+	sl = palloc0(sizeof(SecLabelItem));
+	sl->object.classId = RelationRelationId;
+	sl->object.objectId = InvalidOid;	/* to be assigned later */
+	sl->object.objectSubId = 0;
+	sl->tag = SEPGSQL_LABEL_TAG;
+	sl->seclabel = tcontext;
+
+	seclabels = list_make1(sl);
+
+	if (relkind != RELKIND_RELATION)
+		return seclabels;
+
+	/* permission on the columns */
+	for (index = FirstLowInvalidHeapAttributeNumber + 1;
+		 index < tupdesc->natts;
+		 index++)
+	{
+		Form_pg_attribute	attr;
+
+		if (index == ObjectIdAttributeNumber && !tupdesc->tdhasoid)
+			continue;
+
+		if (index < 0)
+			attr = SystemAttributeDefinition(index, tupdesc->tdhasoid);
+		else
+			attr = tupdesc->attrs[index];
+
+		required = SEPG_DB_COLUMN__CREATE;
+		if (createAs && index >= 0)
+			required |= SEPG_DB_COLUMN__INSERT;
+
+		snprintf(auname, sizeof(auname), "%s.%s", relName, NameStr(attr->attname));
+
+		sepgsql_compute_perms(sepgsql_get_client_label(),
+							  tcontext,
+							  tclass,
+							  required,
+							  auname,
+							  true);
+
+		sl = palloc0(sizeof(SecLabelItem));
+		sl->object.classId = RelationRelationId;
+		sl->object.objectId = InvalidOid;		/* to be assigned later */
+		sl->object.objectSubId = attr->attnum;
+		sl->tag = SEPGSQL_LABEL_TAG;
+		sl->seclabel = tcontext;
+		seclabels = lappend(seclabels, sl);
+	}
+	return seclabels;
+}
+
 /*
  * sepgsql_register_relation_hooks
  *
@@ -341,4 +434,5 @@ void
 sepgsql_register_relation_hooks(void)
 {
 	ExecutorCheckPerms_hook = sepgsql_relation_privileges;
+	check_relation_create_hook = sepgsql_relation_create;
 }
